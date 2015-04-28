2adbbe9f5aceadccbeb3083d2dd2870c0f884dc4
Andrew White <andyw@pixeltrix.co.uk>
2015-04-28 13:02:19 +0100

    Use next for early termination of a proc
    Using break inside of a proc causes the proc's caller to return. Since
    we want the rest of the validations to run use next instead and this also
    prevents a local jump error.

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -48,20 +48,20 @@ class Signature < ActiveRecord::Base
     matcher = Signature.where(:encrypted_email => signature.encrypted_email, :petition_id => signature.petition_id)
     matcher = matcher.where("signatures.id != ?", signature.id) unless signature.new_record?
     existing_email_address_count = matcher.count
-    break if existing_email_address_count == 0
+    next if existing_email_address_count == 0
     if existing_email_address_count > 1
       signature.errors.add(:email, 'This email address is not allowed to sign this petition again')
-      break
+      next
     end
     existing_signature =  matcher.first
     if (existing_signature.name.strip.downcase == signature.name.strip.downcase)
       signature.errors.add(:email, 'You cannot sign this petition again')
-      break
+      next
     end
     if (existing_signature.postcode.gsub(/\s+/,'').downcase !=
         signature.postcode.gsub(/\s+/,'').downcase)
       signature.errors.add(:email, 'This email address is not allowed to sign this petition again')
-      break
+      next
     end
 
   end
