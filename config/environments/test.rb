029a8936c8758a0f0b3d58ea25f610f9a823d35d
Andrew White <andyw@pixeltrix.co.uk>
2015-04-28 13:25:46 +0100

    Set default_url_options for Action Mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Set default_url_options for links in emails
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
   # Randomize the order test cases are executed.
   config.active_support.test_order = :random
 
