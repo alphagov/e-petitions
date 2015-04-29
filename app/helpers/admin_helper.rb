d53ed000e2532ebc272c01bec18a205cd528c252
Andrew White <andyw@pixeltrix.co.uk>
2015-04-29 07:37:57 +0100

    Use UJS (Unobtrusive JavaScript) for confirmations
    Rails now uses UJS for adding things like confirmations to links and
    they need to be applied using data-* style attributes.

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,9 @@
 module AdminHelper
-  
+
   def save_button(resource)
     submit_tag 'Save'
   end
-  
+
   def mandatory_field()
     return '<span class="mandatory">&nbsp;*</span>'
   end
@@ -11,11 +11,12 @@ module AdminHelper
   def cms_delete_link(model, options = {})
     options[:model_name] ||= model.name
     options[:url] ||= resource_path(model)
-    link_to image_tag('admin/delete.png', :size => "16x16", :alt => "Delete"), options[:url],
-                  :confirm => "WARNING: This action cannot be undone.\nAre you sure you want to delete #{h options[:model_name]}?",
-                  :method => :delete
+    link_to image_tag('admin/delete.png', :size => "16x16", :alt => "Delete"), options[:url], :data => {
+                        :confirm => "WARNING: This action cannot be undone.\nAre you sure you want to delete #{h options[:model_name]}?",
+                        :method => :delete
+                      }
   end
-  
+
   def setup_admin_user(admin_user)
     4.times do
       admin_user.departments.build
