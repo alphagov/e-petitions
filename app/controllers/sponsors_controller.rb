1db36ae18e2ce268abd1818c20a8b22482b4bd5c
Murray Steele <muz@h-lame.com>
2015-05-07 18:04:34 +0100

    Add redirects to sponsors controller based on signature presence
    If the signature for the sponsor exists already the show and update actions immediately redirect to the thank-you page.  If the signature for the sponsor does not exist already then the thank-you page redirects to the show action.

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -5,26 +5,37 @@ class SponsorsController < ApplicationController
   respond_to :html
 
   def show
-    @signature = @sponsor.build_signature(:country => "United Kingdom")
+    if @sponsor.signature.nil?
+      @signature = @sponsor.build_signature(:country => "United Kingdom")
+    else
+      redirect_to thank_you_petition_sponsor_path(@petition, token: @sponsor.perishable_token, secure: true)
+    end
   end
 
   def update
-    @signature = @sponsor.create_signature(signature_params_for_create)
+    if @sponsor.signature.nil?
+      @signature = @sponsor.create_signature(signature_params_for_create)
 
-    if @signature.persisted?
-      # If the user has filled in all the correc things then we can
-      # go straight to validated without the email: the sponsor email
-      # that gets them here acts as a validation of their email address
-      @signature.perishable_token = nil
-      @signature.state = Signature::VALIDATED_STATE
-      @signature.save(:validate => false)
-      redirect_to thank_you_petition_sponsor_path(@petition, token: @sponsor.perishable_token, secure: true)
+      if @signature.persisted?
+        # If the user has filled in all the correc things then we can
+        # go straight to validated without the email: the sponsor email
+        # that gets them here acts as a validation of their email address
+        @signature.perishable_token = nil
+        @signature.state = Signature::VALIDATED_STATE
+        @signature.save(:validate => false)
+        redirect_to thank_you_petition_sponsor_path(@petition, token: @sponsor.perishable_token, secure: true)
+      else
+        render :show
+      end
     else
-      render :show
+      redirect_to thank_you_petition_sponsor_path(@petition, token: @sponsor.perishable_token, secure: true)
     end
   end
 
   def thank_you
+    unless @sponsor.signature.present?
+      redirect_to petition_sponsor_path(@petition, token: @sponsor.perishable_token, secure: true)
+    end
   end
 
   private
--- spec/controllers/sponsors_controller_spec.rb
+++ spec/controllers/sponsors_controller_spec.rb
@@ -37,17 +37,46 @@ describe SponsorsController do
       }.to raise_error ActiveRecord::RecordNotFound
     end
 
-    it 'builds a signature for the sponsor' do
-      get :show, petition_id: petition, token: sponsor.perishable_token
-      expect(assigns[:signature]).to be_present
-      expect(assigns[:signature].petition).to eq sponsor.petition
-      expect(assigns[:signature].email).to eq sponsor.email
+    context 'when the sponsor has already signed the petition' do
+      before { sponsor.create_signature!(FactoryGirl.attributes_for(:validated_signature)) }
+
+      it 'redirects to the thank-you page if the sponsor has already signed the petition' do
+        get :show, petition_id: petition, token: sponsor.perishable_token
+        redirect_path = thank_you_petition_sponsor_path(petition, token: sponsor.perishable_token, secure: true)
+        expect(response).to redirect_to redirect_path
+      end
+    end
+
+    context 'when the sponsor has not signed the petition yet' do
+      it 'renders the form' do
+        get :show, petition_id: petition, token: sponsor.perishable_token
+        expect(response).to render_template :show
+      end
+
+      it 'builds a signature for the sponsor' do
+        get :show, petition_id: petition, token: sponsor.perishable_token
+        expect(assigns[:signature]).to be_present
+        expect(assigns[:signature].petition).to eq sponsor.petition
+        expect(assigns[:signature].email).to eq sponsor.email
+      end
     end
   end
 
   context 'PATCH update' do
     let(:petition) { FactoryGirl.create(:petition) }
     let(:sponsor) { FactoryGirl.create(:sponsor, petition: petition) }
+    let(:signature_params) {
+      {
+        name: 'S. Ponsor',
+        address: '1 Sponsor St.',
+        town: 'Sponsorsville',
+        postcode: 'SP1 1NR',
+        country: 'United Kingdom',
+        uk_citizenship: '1',
+        notify_by_email: '0',
+        terms_and_conditions: '1'
+      }
+    }
 
     def do_patch
       patch :update, petition_id: petition,
@@ -56,71 +85,64 @@ describe SponsorsController do
       sponsor.reload
     end
 
-    context 'with valid signature params' do
-      let(:signature_params) {
-        {
-          name: 'S. Ponsor',
-          address: '1 Sponsor St.',
-          town: 'Sponsorsville',
-          postcode: 'SP1 1NR',
-          country: 'United Kingdom',
-          uk_citizenship: '1',
-          notify_by_email: '0',
-          terms_and_conditions: '1'
-        }
-      }
+    context 'when the sponsor has already signed the petition' do
+      before { sponsor.create_signature!(FactoryGirl.attributes_for(:validated_signature, name: 'P. Sonsor')) }
 
-      it 'creates a signature for the sponsor with the supplied params' do
-        do_patch
-        expect(sponsor.signature).to be_present
-        expect(sponsor.signature.name).to eq signature_params[:name]
-        expect(sponsor.signature.postcode).to eq signature_params[:postcode]
-        expect(sponsor.signature.country).to eq signature_params[:country]
-        expect(sponsor.signature.notify_by_email).to eq false
-      end
-
-      it 'creates the signature in the validated state' do
-        do_patch
-        expect(sponsor.signature.state).to eq Signature::VALIDATED_STATE
-        expect(sponsor.signature.perishable_token).to be_nil
-      end
-
-      it 'creates a signature for the sponsor using the email and petition from the sponsor' do
-        do_patch
-        expect(sponsor.signature.email).to eq sponsor.email
-        expect(sponsor.signature.petition).to eq sponsor.petition
-      end
-
-      it 'redirects to the thank you page' do
+      it 'redirects to the thank-you page if the sponsor has already signed the petition' do
         do_patch
         redirect_path = thank_you_petition_sponsor_path(petition, token: sponsor.perishable_token, secure: true)
         expect(response).to redirect_to redirect_path
       end
+
+      it 'does not change the attributes of the existing signature' do
+        do_patch
+        expect(sponsor.signature.name).not_to eq signature_params[:name]
+      end
     end
 
-    context 'with invalid signature params' do
-      let(:signature_params) {
-        {
-          name: 'S. Ponsor',
-          address: '1 Sponsor St.',
-          town: 'Sponsorsville',
-          postcode: 'SP1 1NR',
-          country: 'United Kingdom',
-          uk_citizenship: '1',
-          notify_by_email: '0',
-          terms_and_conditions: '0'
-        }
-      }
+    context 'when the sponsor has not signed the petition yet' do
+      context 'with valid signature params' do
+        it 'creates a signature for the sponsor with the supplied params' do
+          do_patch
+          expect(sponsor.signature).to be_present
+          expect(sponsor.signature.name).to eq signature_params[:name]
+          expect(sponsor.signature.postcode).to eq signature_params[:postcode]
+          expect(sponsor.signature.country).to eq signature_params[:country]
+          expect(sponsor.signature.notify_by_email).to eq false
+        end
 
-      it 'does not persist the signature' do
-        do_patch
-        expect(sponsor.signature).not_to be_present
-        expect(assigns[:signature]).not_to be_persisted
+        it 'creates the signature in the validated state' do
+          do_patch
+          expect(sponsor.signature.state).to eq Signature::VALIDATED_STATE
+          expect(sponsor.signature.perishable_token).to be_nil
+        end
+
+        it 'creates a signature for the sponsor using the email and petition from the sponsor' do
+          do_patch
+          expect(sponsor.signature.email).to eq sponsor.email
+          expect(sponsor.signature.petition).to eq sponsor.petition
+        end
+
+        it 'redirects to the thank you page' do
+          do_patch
+          redirect_path = thank_you_petition_sponsor_path(petition, token: sponsor.perishable_token, secure: true)
+          expect(response).to redirect_to redirect_path
+        end
       end
 
-      it 'renders the form again' do
-        do_patch
-        expect(response).to render_template :show
+      context 'with invalid signature params' do
+        before { signature_params[:terms_and_conditions] = '0' }
+
+        it 'does not persist the signature' do
+          do_patch
+          expect(sponsor.signature).not_to be_present
+          expect(assigns[:signature]).not_to be_persisted
+        end
+
+        it 'renders the form again' do
+          do_patch
+          expect(response).to render_template :show
+        end
       end
     end
   end
@@ -162,6 +184,17 @@ describe SponsorsController do
       expect {
         get :thank_you, petition_id: petition_2, token: sponsor.perishable_token
       }.to raise_error ActiveRecord::RecordNotFound
+    end
+
+    it 'renders the view if the sponsor has signed the petition' do
+      get :thank_you, petition_id: petition, token: sponsor.perishable_token
+      expect(response).to render_template :thank_you
+    end
+
+    it 'redirects to show if the sponsor has not signed the petition' do
+      signature.destroy
+      get :thank_you, petition_id: petition, token: sponsor.perishable_token
+      expect(response).to redirect_to petition_sponsor_path(petition, token: sponsor.perishable_token, secure: true)
     end
   end
 end
