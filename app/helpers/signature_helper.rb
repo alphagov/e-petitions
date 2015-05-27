23794a8dd86d112722593ba019225641b255fbf2
Murray Steele <muz@h-lame.com>
2015-05-27 11:05:36 +0100

    Allow passing options when rendering the signature form
    In some cases we're not going to want the stage 1 back link (e.g. when using this UI for sponsors).

--- app/helpers/signature_helper.rb
+++ app/helpers/signature_helper.rb
@@ -1,28 +1,28 @@
 module SignatureHelper
-  def render_signature_form(stage_manager, form)
+  def render_signature_form(stage_manager, form, options = {})
     capture do
-      concat render_signature_hidden_details(stage_manager, form)
-      concat render_signature_ui(stage_manager, form)
+      concat render_signature_hidden_details(stage_manager, form, options)
+      concat render_signature_ui(stage_manager, form, options)
     end
   end
 
   private
 
-  def render_signature_hidden_details(stage_manager, form)
+  def render_signature_hidden_details(stage_manager, form, options)
     capture do
       concat hidden_field_tag(:stage, stage_manager.stage)
-      concat render('/signatures/create/signer_hidden', signature: stage_manager.stage_object, f: form) unless stage_manager.stage == 'signer'
+      concat render('/signatures/create/signer_hidden', options.merge(signature: stage_manager.stage_object, f: form)) unless stage_manager.stage == 'signer'
     end
   end
 
-  def render_signature_ui(stage_manager, form)
+  def render_signature_ui(stage_manager, form, options)
     # NOTE: make sure we skip past the existing tabindex-ed elements on the page, no matter which ui we render
     increment(4)
     case stage_manager.stage
     when 'signer'
-      render('/signatures/create/signer_ui', signature: stage_manager.stage_object, f: form)
+      render('/signatures/create/signer_ui', options.merge(signature: stage_manager.stage_object, f: form))
     when 'replay-email'
-      render('/signatures/create/replay_email_ui', signature: stage_manager.stage_object, f: form)
+      render('/signatures/create/replay_email_ui', options.merge(signature: stage_manager.stage_object, f: form))
     end
   end
 end
