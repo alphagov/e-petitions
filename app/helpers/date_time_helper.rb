fec48284e0412d8d0c726d7a29e68b045faadbfc
Andrew White <andyw@pixeltrix.co.uk>
2015-06-02 19:55:58 +0100

    Remove redundant time format
    The dotted_short_date format has no dots and is only used in one place
    so there's no point adding it to Time::FORMATS.

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -1,28 +1,25 @@
 module DateTimeHelper
-
   def short_date_format(date_time)
-    date_time.strftime("%e %B %Y") if date_time
+    date_time && date_time.strftime("%e %B %Y")
   end
 
   def date_time_format(date_time)
-    date_time.strftime("%d-%m-%Y %H:%M") if date_time
+    date_time && date_time.strftime("%d-%m-%Y %H:%M")
   end
 
   def date_format(date_time)
-    date_time.to_s(:dotted_short_date) if date_time
+    date_time && date_time.strftime("%d/%m/%Y")
   end
 
   def date_format_admin(date_time)
-    date_time.strftime("%d-%m-%Y") if date_time
+    date_time && date_time.strftime("%d-%m-%Y")
   end
 
   def local_date_time_format(date_time)
-    return unless date_time
-    date_time.in_time_zone.strftime("%d/%m/%Y %H:%M")
+    date_time && date_time.in_time_zone.strftime("%d/%m/%Y %H:%M")
   end
 
   def last_updated_at_time(date_time)
-    return unless date_time
-    date_time.in_time_zone.strftime("%H:%M %Z")
+    date_time && date_time.in_time_zone.strftime("%H:%M %Z")
   end
 end
