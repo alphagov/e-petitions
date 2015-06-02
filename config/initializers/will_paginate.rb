5de09f8b194b7d94be3c2775f1763f4b69a28f11
Andrew White <andyw@pixeltrix.co.uk>
2015-06-02 19:30:52 +0100

    Use locale for customising Will Paginate
    Using the pagination options hash for Will Paginate to customise labels
    is deprecated so switch to using a locale file going forward.

--- config/initializers/will_paginate.rb
+++ config/initializers/will_paginate.rb
@@ -1,5 +1,2 @@
 WillPaginate::ViewHelpers.pagination_options[:inner_window] = 3
 WillPaginate::ViewHelpers.pagination_options[:outer_window] = 1
-# Remove arrows from previous/next labels
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Previous'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Next'
