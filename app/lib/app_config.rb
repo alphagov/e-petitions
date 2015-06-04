0fc854ff31329818665b09758b450f9d199b0c6a
Andrew White <andyw@pixeltrix.co.uk>
2015-06-04 22:14:17 +0100

    Reimplent AppConfig using Singleton
    Using class variables (e.g. @@config) has some thread safety issues so
    switch to the singleton library which should be more resilent when running
    with multiple threads in puma.
    
    Also tided up the application.yml file and added support for using ERB
    blocks inside it - primarily to fetch config from environment variables.

--- app/lib/app_config.rb
+++ app/lib/app_config.rb
@@ -1,25 +1,37 @@
+require 'singleton'
+
 class AppConfig
+  include Singleton
+
   class << self
-    @@config = nil
-
-    protected
-    def config
-      @@config ||= YAML.load_file(Rails.root.join(*%w(config application.yml)))[Rails.env]
-    end
-
-    public
-
-    def has_setting?(name)
-      self.config.has_key?(name.to_s)
-    end
-
     def method_missing(name, *args)
-      if has_setting?(name.to_s)
-        self.config[name.to_s]
+      if instance.respond_to?(name)
+        instance.send(name, *args)
       else
         super
       end
     end
+  end
 
+  def respond_to?(name, *args)
+    config.key?(name.to_s)
+  end
+
+  def method_missing(name, *args)
+    config.key?(name.to_s) ? config[name.to_s] : super
+  end
+
+  private
+
+  def config
+    @config ||= load_app_config[Rails.env]
+  end
+
+  def load_app_config
+    YAML.load(ERB.new(yaml).result)
+  end
+
+  def yaml
+    File.read(Rails.root.join(*%w[config application.yml]))
   end
 end
