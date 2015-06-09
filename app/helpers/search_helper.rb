3dc8d8cfb504bf3b38a802d22cb210855aefbdb2
Otto Laitila <otto.laitila@unboxedconsulting.com>
2015-06-09 17:48:19 +0100

    Add custom pagination

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -7,6 +7,17 @@ module SearchHelper
     safe_join(search_list_links)
   end
 
+  def will_paginate_petitions(collection_or_options = nil, options = {})
+    options[:page_links] = false
+    options[:previous_label] =
+      "<span class='icon icon-paginate-previous paginate paginate-previous' aria-hidden='true'></span>
+       <span class='paginate paginate-previous'>Previous</span>"
+    options[:next_label] =
+      "<span class='icon icon-paginate-next paginate paginate-next' aria-hidden='true'></span>
+       <span class='paginate paginate-next'>Next</span>"
+    will_paginate *[collection_or_options, options].compact
+  end
+
   private
 
   def petition_list_url(state, params)
