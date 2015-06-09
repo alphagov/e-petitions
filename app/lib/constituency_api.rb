7773c3625183f9ed2bf89e44db1ea5589257cb2d
David Basalla <davidbasalla@gmail.com>
2015-06-09 12:28:59 +0100

    Add mp parsing to constituency API
    Rebased from master

--- app/lib/constituency_api.rb
+++ app/lib/constituency_api.rb
@@ -1,25 +1,32 @@
 module ConstituencyApi
   ConstituencyApiError = Class.new(RuntimeError)
 
-  class Constituency < Struct.new(:name)
+  class Constituency < Struct.new(:name, :mp)
   end
-  
+
+  class Mp < Struct.new(:id, :name, :start_date)
+    URL = "http://www.parliament.uk/biographies/commons"
+    def url
+      "#{URL}/#{name.parameterize}/#{id}"
+    end
+  end
+
   class Client
     include Faraday
     URL = 'http://data.parliament.uk/membersdataplatform/services/mnis/Constituencies'
     TIMEOUT = 5
-    
+
     def self.constituencies(postcode)
       response = call_api(postcode)
       parse_constituencies(response)
     end
-    
+
     def self.parse_constituencies(response)
       return [] unless response["Constituencies"]
       constituencies = response["Constituencies"]["Constituency"]
-      Array.wrap(constituencies).map { |c| Constituency.new(c["Name"]) }
+      Array.wrap(constituencies).map { |c| Constituency.new(c["Name"], last_mp(c)) }
     end
-    
+
     def self.call_api(postcode)
       response = Faraday.new(URL).get("#{postcode_param(postcode)}/") do |req|
         req.options[:timeout] = TIMEOUT
@@ -37,12 +44,22 @@ module ConstituencyApi
     rescue Faraday::Error => e
       raise ConstituencyApiError.new("Network error - #{e}")
     end
-    
+
+    def self.last_mp(constituency_hash)
+      mps = parse_mps(constituency_hash)
+      mps.sort_by {|mp| mp.start_date}.last
+    end
+
+    def self.parse_mps(response)
+      return nil unless response["RepresentingMembers"]
+      mps = response["RepresentingMembers"]["RepresentingMember"]
+      Array.wrap(mps).map { |m| Mp.new(m["Member_Id"].to_i, m["Member"], m["StartDate"].to_date) }
+    end
+
     def self.postcode_param(postcode)
       postcode.gsub(/\s+/, "")
     end
     private_class_method :parse_constituencies, :call_api, :postcode_param
   end
 end
-
 
