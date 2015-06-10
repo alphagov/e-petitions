f161bea93e6c1635027e4e62a56f1ffa50e3e40b
Murray Steele <muz@h-lame.com>
2015-06-10 11:53:14 +0100

    Add .for and #record_new_signature to ConstituencyPetitionJournal
    These two methods should act as the total public api for interacting with this model (short of looking at the attributes).
    
    * ``.for`` allows us to fetch the journal for a petition and constituency and it will make sure we always get a result that comes out of the db if one didn't exist prior to calling the method.
    * ``#record_new_signature`` increments the ``signature_count`` but tries to do it in such a way that it should avoid race conditions etc.  e.g. by doing the whole increment with a SQL query, rather than an in-memory increment and saving that down to the db.  So if an increment happens between us loading the object and calling ``#record_new_signature`` on it we won't lose the intermediate increment.

--- app/models/constituency_petition_journal.rb
+++ app/models/constituency_petition_journal.rb
@@ -5,4 +5,15 @@ class ConstituencyPetitionJournal < ActiveRecord::Base
   validates :constituency_id, presence: true, length: { maximum: 255 }
   validates :petition_id, uniqueness: { scope: [:constituency_id] }
   validates :signature_count, presence: true
+
+  def self.for(petition, constituency_id)
+    find_or_create_by(petition: petition, constituency_id: constituency_id)
+  end
+
+  def record_new_signature(at = Time.current)
+    signature_count_field = self.class.connection.quote_column_name('signature_count')
+    changes = "#{signature_count_field} = #{signature_count_field} + 1, updated_at = :updated_at"
+    self.class.where(id: id).update_all([changes, updated_at: at])
+    reload
+  end
 end
--- spec/models/constituency_petition_journal_spec.rb
+++ spec/models/constituency_petition_journal_spec.rb
@@ -21,4 +21,73 @@ RSpec.describe ConstituencyPetitionJournal, type: :model do
     it { is_expected.to validate_uniqueness_of(:petition_id).scoped_to(:constituency_id) }
     it { is_expected.to validate_presence_of(:signature_count) }
   end
+
+  context '.for' do
+    let(:petition) { FactoryGirl.create(:petition) }
+    let(:constituency_id) { FactoryGirl.generate(:constituency_id) }
+
+    context 'when there is a journal for the requested petition and constituency' do
+      let!(:existing_record) { FactoryGirl.create(:constituency_petition_journal, petition: petition, constituency_id: constituency_id, signature_count: 30) }
+
+      it 'doesn\'t create a new record' do
+        expect {
+          described_class.for(petition, constituency_id)
+        }.not_to change(described_class, :count)
+      end
+
+      it 'fetches the instance from the DB' do
+        fetched = described_class.for(petition, constituency_id)
+        expect(fetched).to eq existing_record
+      end
+    end
+
+    context 'when there is no journal for the requested petition and constituency' do
+      it 'creates a new instance in the DB' do
+        expect {
+          described_class.for(petition, constituency_id)
+        }.to change(described_class, :count).by(1)
+      end
+
+      it 'returns the newly created instance' do
+        fetched = described_class.for(petition, constituency_id)
+        expect(fetched).to be_a described_class
+        expect(fetched).to be_persisted
+      end
+
+      it 'sets the petition of the new instance to the supplied petition' do
+        fetched = described_class.for(petition, constituency_id)
+        expect(fetched.petition).to eq petition
+      end
+
+      it 'sets the constituency_id of the new instance to the supplied petition' do
+        fetched = described_class.for(petition, constituency_id)
+        expect(fetched.constituency_id).to eq constituency_id
+      end
+
+      it 'has 0 for a signature count' do
+        fetched = described_class.for(petition, constituency_id)
+        expect(fetched.signature_count).to eq 0
+      end
+    end
+  end
+
+  context '#record_new_signature' do
+    let(:petition) { FactoryGirl.create(:petition) }
+    let(:constituency_id) { FactoryGirl.generate(:constituency_id) }
+
+    subject { described_class.for(petition, constituency_id) }
+
+    it 'increments signature_count by 1' do
+      expect {
+        subject.record_new_signature
+      }.to change(subject, :signature_count).by(1)
+    end
+
+    it 'persists the change' do
+      old_signature_count = subject.signature_count
+      subject.record_new_signature
+      subject.reload
+      expect(subject.signature_count).not_to eq old_signature_count
+    end
+  end
 end
