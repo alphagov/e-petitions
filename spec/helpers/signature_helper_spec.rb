b79015802f04ccc62a2afc0fc29460621eaa18e3
Andrew White <andyw@pixeltrix.co.uk>
2015-06-11 18:13:46 +0100

    Use locale keys to correctly pluralize signature counts
    The i18n gem has a feature where if you pass it an option called count
    then it will use the sub key called one if the value of count is 1 and
    if the value of count is more than 1 then it uses other. Coupled with
    the use of the html key to mark the output as HTML-safe allows us to
    localize the language and formatting if needed at some point.

---
+++ spec/helpers/signature_helper_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe SearchHelper, type: :helper do
+  describe "#signature_count" do
+    describe "for normal petition lists" do
+      it "returns a HTML-safe string" do
+        expect(helper.signature_count(:default, 1)).to be_an(ActiveSupport::SafeBuffer)
+      end
+
+      context "when the signature count is 1" do
+        it "returns a correctly formatted signature count" do
+          expect(helper.signature_count(:default, 1)).to eq("1 <span>signature</span>")
+        end
+      end
+
+      context "when the signature count is 100" do
+        it "returns a correctly formatted signature count" do
+          expect(helper.signature_count(:default, 100)).to eq("100 <span>signatures</span>")
+        end
+      end
+
+      context "when the signature count is 1000" do
+        it "returns a correctly formatted signature count" do
+          expect(helper.signature_count(:default, 1000)).to eq("1,000 <span>signatures</span>")
+        end
+      end
+    end
+
+    describe "for trending petition lists" do
+      it "returns a HTML-safe string" do
+        expect(helper.signature_count(:trending, 1)).to be_an(ActiveSupport::SafeBuffer)
+      end
+
+      context "when the signature count is 1" do
+        it "returns a correctly formatted signature count" do
+          expect(helper.signature_count(:trending, 1)).to eq("<span class=\"count\">1</span> signature in the last hour")
+        end
+      end
+
+      context "when the signature count is 100" do
+        it "returns a correctly formatted signature count" do
+          expect(helper.signature_count(:trending, 100)).to eq("<span class=\"count\">100</span> signatures in the last hour")
+        end
+      end
+
+      context "when the signature count is 1000" do
+        it "returns a correctly formatted signature count" do
+          expect(helper.signature_count(:trending, 1000)).to eq("<span class=\"count\">1,000</span> signatures in the last hour")
+        end
+      end
+    end
+  end
+end
