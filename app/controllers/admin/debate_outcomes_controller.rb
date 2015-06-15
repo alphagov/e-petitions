7c7f28cdd156830f76831ddeca5efc4404ffe82e
Murray Steele <muz@h-lame.com>
2015-06-15 17:25:56 +0100

    Implement ``DebateOutcomesController#update``
    Not much to say.

--- app/controllers/admin/debate_outcomes_controller.rb
+++ app/controllers/admin/debate_outcomes_controller.rb
@@ -1,12 +1,33 @@
 class Admin::DebateOutcomesController < Admin::AdminController
   respond_to :html
+  before_action :fetch_petition
 
   def show
-    @petition = Petition.find(params[:petition_id])
-    if @petition.can_have_debate_added?
+    fetch_debate_outcome
+  end
 
+  def update
+    fetch_debate_outcome
+    if @debate_outcome.update_attributes(params_for_update)
+      redirect_to [:admin, @petition]
     else
-      raise ActiveRecord::RecordNotFound
+      render :show
     end
+  end
+
+  private
+  def fetch_petition
+    @petition = Petition.find(params[:petition_id])
+    raise ActiveRecord::RecordNotFound unless @petition.can_have_debate_added?
+  end
+
+  def fetch_debate_outcome
+    @debate_outcome = @petition.debate_outcome || @petition.build_debate_outcome
+  end
+
+  def params_for_update
+    params.
+      require(:debate_outcome).
+      permit(:debated_on, :overview, :transcript_url, :video_url)
   end
 end
