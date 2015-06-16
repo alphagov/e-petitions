4c525e8579dc7e138492c3c78636f186cd92408d
Andrew White <andyw@pixeltrix.co.uk>
2015-06-16 05:17:54 +0100

    Provide a default host for the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
+  # Set default_url_options for links in emails
+  config.action_mailer.default_url_options = { host: ENV.fetch('EPETITIONS_HOST') }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
