2a51463ba72a23bb727a7dba210a92e021de457c
Andrew White <andyw@pixeltrix.co.uk>
2015-06-17 18:24:31 +0100

    Move email subjects to locale file
    As part of the ongoing process to move stuff to the locale file add keys
    for the petition email subjects. There is a built-in method in Rails to
    translate subjects but that's tied to the mailer class and action name.

--- app/mailers/petition_mailer.rb
+++ app/mailers/petition_mailer.rb
@@ -3,77 +3,82 @@ class PetitionMailer < ApplicationMailer
 
   def email_confirmation_for_signer(signature)
     @signature = signature
-    mail(:subject => "HM Government e-petitions: Email address confirmation", :to => @signature.email)
+    mail to: @signature.email, subject: subject_for(:email_confirmation_for_signer)
   end
 
   def special_resend_of_email_confirmation_for_signer(signature)
     @signature = signature
-    mail(:subject => "HM Government e-petitions: Email address confirmation", :to => @signature.email)
+    mail to: @signature.email, subject: subject_for(:special_resend_of_email_confirmation_for_signer)
   end
 
   def notify_creator_that_petition_is_published(signature)
     @signature = signature
-    mail(:subject => "HM Government e-petitions: Your e-petition has been published", :to => @signature.email)
+    mail to: @signature.email, subject: subject_for(:notify_creator_that_petition_is_published)
   end
 
   def petition_rejected(petition)
     @petition = petition
     to = @petition.creator_signature.email
     bcc = @petition.sponsor_signatures.validated.map(&:email)
-    mail(:subject => "HM Government e-petitions: e-petition has been rejected", :to => to, :bcc => bcc)
+
+    mail to: to, bcc: bcc, subject: subject_for(:petition_rejected)
   end
 
   def notify_signer_of_threshold_response(petition, signature)
-    @petition = petition
-    @signature = signature
-    mail(:subject => "HM Government e-petitions: The petition '#{petition.title}' has reached #{number_with_delimiter(petition.signature_count)} signatures", :to => @signature.email)
+    @petition, @signature = petition, signature
+    mail to: @signature.email, subject: subject_for(:notify_signer_of_threshold_response)
   end
 
   def no_signature_for_petition(petition, email)
     @petition = petition
-    mail  :subject => "HM Government e-petitions: a confirmation email has been requested",
-          :to      => email
+    mail to: email, subject: subject_for(:no_signature_for_petition)
   end
 
   def email_already_confirmed_for_signature(signature)
     @signature = signature
-    mail  :subject => "HM Government e-petitions: Signature already confirmed",
-          :to      => @signature.email
+    mail to: @signature.email, subject: subject_for(:email_already_confirmed_for_signature)
   end
 
   def two_pending_signatures(signature_one, signature_two)
-    @signature_one = signature_one
-    @signature_two = signature_two
-    mail  :subject => "HM Government e-petitions: Signature confirmations",
-          :to      => @signature_one.email
+    @signature_one, @signature_two = signature_one, signature_two
+    mail to: @signature_one.email, subject: subject_for(:two_pending_signatures)
   end
 
   def one_pending_one_validated_signature(pending_signature, validated_signature)
-    @pending_signature   = pending_signature
-    @validated_signature = validated_signature
-    mail  :subject => "HM Government e-petitions: Signature confirmation",
-          :to      =>  @pending_signature.email
+    @pending_signature, @validated_signature = pending_signature, validated_signature
+    mail to: @pending_signature.email, subject: subject_for(:one_pending_one_validated_signature)
   end
 
   def double_signature_confirmation(signatures)
-    @signature_one = signatures.first
-    @signature_two = signatures.second
-    mail  :subject => "HM Government e-petitions: Signatures already confirmed",
-          :to      => @signature_one.email
+    @signature_one, @signature_two = signatures.first, signatures.second
+    mail to: @signature_one.email, subject: subject_for(:double_signature_confirmation)
   end
 
   def notify_creator_of_closing_date_change(signature)
     @signature = signature
-    mail :subject => "HM Government e-petitions: change to your e-petition closing date",
-         :to      => @signature.email
+    mail to: @signature.email, subject: subject_for(:notify_creator_of_closing_date_change)
   end
 
   def gather_sponsors_for_petition(petition)
-    @petition = petition
-    @creator = petition.creator_signature
-    mail(
-      subject: "Parliament petitions - It's time to get sponsors to support your petition",
-      to: @creator.email
-    )
+    @petition, @creator = petition, petition.creator_signature
+    mail to: @creator.email, subject: subject_for(:gather_sponsors_for_petition)
+  end
+
+  private
+
+  def subject_for(key, options = {})
+    I18n.t key, i18n_options.merge(options)
+  end
+
+  def i18n_options
+    {}.tap do |options|
+      options[:scope] = :"petitions.emails.subjects"
+
+      if defined?(@petition)
+        options[:count] = @petition.signature_count
+        options[:formatted_count] = number_to_delimited(@petition.signature_count)
+        options[:title] = @petition.title
+      end
+    end
   end
 end
