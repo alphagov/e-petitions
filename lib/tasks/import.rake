alphagov/e-petitions

commit 02627f966fbdfe12c640eeafd82a5f26c0f7c5a0

Switch archived petitions to use the textacular gem
Because textacular adds a ranking column by default we need to re-select
the standard set of attributes to overcome the performance penalty of
calculating that ranking algorithm especially for large result sets. We
also need re-order the query to remove the ranking column.

lib/tasks/import.rake
@@ -28,33 +28,31 @@ namespace :import do
       ->(value) { value =~ /\A\d+\z/ ? Integer(value) : value }
     ]
 
-    Sunspot.batch do
-      CSV.foreach(file, headers: true, converters: converters) do |row|
-        if ArchivedPetition::STATES.include?(row['state'])
-          petition = ArchivedPetition.find_or_initialize_by(id: row['id'])
+    CSV.foreach(file, headers: true, converters: converters) do |row|
+      if ArchivedPetition::STATES.include?(row['state'])
+        petition = ArchivedPetition.find_or_initialize_by(id: row['id'])
 
-          petition.title             = row['title']
-          petition.description       = row['description']
-          petition.response          = row['response']
-          petition.state             = row['state']
-          petition.opened_at         = row['open_at']
-          petition.closed_at         = row['closed_at']
-          petition.signature_count   = row['signature_count']
+        petition.title             = row['title']
+        petition.description       = row['description']
+        petition.response          = row['response']
+        petition.state             = row['state']
+        petition.opened_at         = row['open_at']
+        petition.closed_at         = row['closed_at']
+        petition.signature_count   = row['signature_count']
 
-          if petition.rejected?
-            if row['rejection_text'].blank?
-              petition.reason_for_rejection = rejection_reasons[row['rejection_code']]
-            else
-              petition.reason_for_rejection = "#{rejection_reasons[row['rejection_code']]}\n\nThe following explanatory notes have been added:\n\n#{row['rejection_text']}"
-            end
+        if petition.rejected?
+          if row['rejection_text'].blank?
+            petition.reason_for_rejection = rejection_reasons[row['rejection_code']]
+          else
+            petition.reason_for_rejection = "#{rejection_reasons[row['rejection_code']]}\n\nThe following explanatory notes have been added:\n\n#{row['rejection_text']}"
           end
-
-          petition.created_at = row['created_at']
-          petition.updated_at = row['updated_at']
-
-          petition.save!
         end
       end
+
+      petition.created_at = row['created_at']
+      petition.updated_at = row['updated_at']
+
+      petition.save!
     end
   end
 end

app/decorators/petition_search.rb
@@ -47,7 +47,7 @@ class PetitionSearch
   end
 
   def execute_search_query
-    Petition.search do |query|
+    Petition.solr_search do |query|
       query.fulltext search_term_sanitised
       query.paginate page: @params[:page], per_page: 50
       case state
@@ -65,7 +65,7 @@ class PetitionSearch
   end
 
   def execute_search_for_create_petition_query
-    Petition.search do |query|
+    Petition.solr_search do |query|
       query.fulltext search_term_sanitised
       query.paginate page: 1, per_page: 3
       query.with(:state, ['open', 'rejected'])
@@ -74,7 +74,7 @@ class PetitionSearch
   end
 
   def execute_result_counts_query
-    Petition.search do |query|
+    Petition.solr_search do |query|
       query.fulltext search_term_sanitised
       query.facet(:state) do
         row(:open) do

app/models/archived_petition.rb
@@ -1,3 +1,5 @@
+require 'textacular/searchable'
+
 class ArchivedPetition < ActiveRecord::Base
   OPEN_STATE = 'open'
   REJECTED_STATE = 'rejected'
@@ -7,22 +9,18 @@ class ArchivedPetition < ActiveRecord::Base
   validates :description, presence: true, length: { maximum: 1000 }
   validates :state, presence: true, inclusion: STATES
 
-  searchable do
-    text :title
-    text :description
-    time :created_at, trie: true
-  end
+  extend Searchable(:title, :description)
 
   class << self
     def search(params)
       query = params[:q].to_s
       page  = [params[:page].to_i, 1].max
 
-      solr_search do
-        fulltext query
-        paginate page: page, per_page: 20
-        order_by 'created_at', 'asc'
-      end.results
+      basic_search(query).
+        except(:select).
+        select(arel_table[Arel.star]).
+        reorder(:created_at).
+        paginate(page: page, per_page: 20)
     end
   end
 

db/migrate/20150603112821_add_search_indexes_for_archived_petitions.rb
@@ -1,0 +1,32 @@
+class AddSearchIndexesForArchivedPetitions < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE INDEX index_archived_petitions_on_title
+      ON archived_petitions
+      USING gin(to_tsvector('english', title));
+    SQL
+
+    execute <<-SQL
+      CREATE INDEX index_archived_petitions_on_description
+      ON archived_petitions
+      USING gin(to_tsvector('english', description));
+    SQL
+
+    execute <<-SQL
+      CREATE INDEX index_archived_petitions_on_state_and_closed_at
+      ON archived_petitions USING btree (state, closed_at);
+    SQL
+
+    execute <<-SQL
+      CREATE INDEX index_archived_petitions_on_signature_count
+      ON archived_petitions USING btree (signature_count);
+    SQL
+  end
+
+  def down
+    execute "DROP INDEX index_archived_petitions_on_title;"
+    execute "DROP INDEX index_archived_petitions_on_description;"
+    execute "DROP INDEX index_archived_petitions_on_state;"
+    execute "DROP INDEX index_archived_petitions_on_signature_count;"
+  end
+end

db/migrate/20150605100049_add_search_indexes_for_petitions.rb
@@ -1,0 +1,24 @@
+class AddSearchIndexesForPetitions < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE INDEX index_petitions_on_title
+      ON petitions USING gin(to_tsvector('english', title));
+    SQL
+
+    execute <<-SQL
+      CREATE INDEX index_petitions_on_action
+      ON petitions USING gin(to_tsvector('english', description));
+    SQL
+
+    execute <<-SQL
+      CREATE INDEX index_petitions_on_description
+      ON petitions USING gin(to_tsvector('english', description));
+    SQL
+  end
+
+  def down
+    execute "DROP INDEX index_petitions_on_title;"
+    execute "DROP INDEX index_petitions_on_action;"
+    execute "DROP INDEX index_petitions_on_description;"
+  end
+end

db/structure.sql
@@ -432,6 +432,34 @@ CREATE INDEX index_admin_users_on_last_name_and_first_name ON admin_users USING 
 
 
 --
+-- Name: index_archived_petitions_on_description; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_archived_petitions_on_description ON archived_petitions USING gin (to_tsvector('english'::regconfig, description));
+
+
+--
+-- Name: index_archived_petitions_on_signature_count; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_archived_petitions_on_signature_count ON archived_petitions USING btree (signature_count);
+
+
+--
+-- Name: index_archived_petitions_on_state_and_closed_at; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_archived_petitions_on_state_and_closed_at ON archived_petitions USING btree (state, closed_at);
+
+
+--
+-- Name: index_archived_petitions_on_title; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_archived_petitions_on_title ON archived_petitions USING gin (to_tsvector('english'::regconfig, (title)::text));
+
+
+--
 -- Name: index_delayed_jobs_on_priority_and_run_at; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -439,10 +467,24 @@ CREATE INDEX index_delayed_jobs_on_priority_and_run_at ON delayed_jobs USING btr
 
 
 --
+-- Name: index_petitions_on_action; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_petitions_on_action ON petitions USING gin (to_tsvector('english'::regconfig, description));
+
+
+--
 -- Name: index_petitions_on_creator_signature_id; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE UNIQUE INDEX index_petitions_on_creator_signature_id ON petitions USING btree (creator_signature_id);
+
+
+--
+-- Name: index_petitions_on_description; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_petitions_on_description ON petitions USING gin (to_tsvector('english'::regconfig, description));
 
 
 --
@@ -464,6 +506,13 @@ CREATE INDEX index_petitions_on_state_and_created_at ON petitions USING btree (s
 --
 
 CREATE INDEX index_petitions_on_state_and_signature_count ON petitions USING btree (state, signature_count);
+
+
+--
+-- Name: index_petitions_on_title; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_petitions_on_title ON petitions USING gin (to_tsvector('english'::regconfig, (title)::text));
 
 
 --
@@ -531,4 +580,8 @@ SET search_path TO "$user",public;
 INSERT INTO schema_migrations (version) VALUES ('20150602200239');
 
 INSERT INTO schema_migrations (version) VALUES ('20150603033108');
+
+INSERT INTO schema_migrations (version) VALUES ('20150603112821');
+
+INSERT INTO schema_migrations (version) VALUES ('20150605100049');
 

commit 1af390c27980d4330d1ff721d7663acec725fd2c

Fix archived petition import task
The import task for archived petitions was broken first by 02627f9 and
then further broken by 210030d and 1873764 - this reverts the changes
made by those commits.

lib/tasks/import.rake
@@ -32,8 +32,8 @@ namespace :import do
       if ArchivedPetition::STATES.include?(row['state'])
         petition = ArchivedPetition.find_or_initialize_by(id: row['id'])
 
-        petition.action             = row['title']
-        petition.additional_details = row['description']
+        petition.title              = row['title']
+        petition.description        = row['description']
         petition.response           = row['response']
         petition.state              = row['state']
         petition.opened_at          = row['open_at']
@@ -47,12 +47,12 @@ namespace :import do
             petition.reason_for_rejection = "#{rejection_reasons[row['rejection_code']]}\n\nThe following explanatory notes have been added:\n\n#{row['rejection_text']}"
           end
         end
+
+        petition.created_at = row['created_at']
+        petition.updated_at = row['updated_at']
+
+        petition.save!
       end
-
-      petition.created_at = row['created_at']
-      petition.updated_at = row['updated_at']
-
-      petition.save!
     end
   end
 end