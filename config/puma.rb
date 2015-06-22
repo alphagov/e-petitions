e06fb1d963cc831810409117d467b7d5f18a12c6
Andrew White <andyw@pixeltrix.co.uk>
2015-06-22 18:04:19 +0100

    Tweak settings for production
    The default number of threads for Puma is 16 so we need to increase the
    number of database connections available. Also NewRelic fires up some
    threads when the app is preloaded so use manual starting to avoid the
    warnings generated by Puma.

--- config/puma.rb
+++ config/puma.rb
@@ -9,6 +9,9 @@ workers ENV.fetch('WEB_CONCURRENCY') { 2 }.to_i
 preload_app!
 
 on_worker_boot do
+  # Manually start NewRelic agent as Puma detects already running threads during boot
+  NewRelic::Agent.manual_start if defined? NewRelic
+
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
