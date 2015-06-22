8e8f5b90baf6a494e9b49d7a3635148e48ac59c5
David Basalla <davidbasalla@gmail.com>
2015-06-22 18:10:24 +0100

    Update admin controllers to handle search

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -6,15 +6,17 @@ class Admin::SearchesController < Admin::AdminController
 
   def result
     @query = params[:search][:query]
-    if (/^\d+$/ =~ @query)
-      find_by_id(@query)
+    if is_number?(@query)
+      find_petition_by_id(@query)
+    elsif is_email?(@query)
+      find_signatures_by_email(@query)
     else
-      @signatures = Signature.for_email(@query).paginate(:page => params[:page], :per_page => 20)
+      find_petitions_by_keyword(@query)
     end
   end
 
   private
-  def find_by_id(query)
+  def find_petition_by_id(query)
     begin
       petition = Petition.find(query.to_i)
       redirect_to url_for_petition_state(petition)
@@ -24,6 +26,14 @@ class Admin::SearchesController < Admin::AdminController
     end
   end
 
+  def find_signatures_by_email(query)
+    @signatures = Signature.for_email(query).paginate(:page => params[:page], :per_page => 20)
+  end
+
+  def find_petitions_by_keyword(query)
+    redirect_to(controller: 'admin/petitions', action: 'index', q: query)
+  end
+  
   def url_for_petition_state(petition)
     return admin_petition_url(petition) unless petition.editable_by?(current_user)
     return admin_petition_url(petition) if petition.awaiting_moderation?
@@ -31,5 +41,13 @@ class Admin::SearchesController < Admin::AdminController
     if (petition.response_editable_by?(current_user))
       edit_response_admin_petition_url(petition)
     end
+  end
+
+  def is_number?(query)
+    /^\d+$/ =~ query
+  end
+
+  def is_email?(query)
+    query.include?('@')
   end
 end
