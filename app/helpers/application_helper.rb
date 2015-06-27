95c9b9b2e6f2917db6567e00e5c4e0b8437eaffe
Andrew White <andyw@pixeltrix.co.uk>
2015-06-27 10:06:02 +0100

    Remove obsolete helper methods
    These helpers are no longer used anywhere in the application.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,4 @@ module ApplicationHelper
     @counter ||= 0
     @counter += amount
   end
-
-  def new_window_link_to(label, path, options = {})
-    link_to raw(label + ' <span class="new_window_icon">This link opens in a new window</span>'), path, options.merge(:target => '_blank')
-  end
-
-  def info_link_to(path, options = {})
-    options[:class] = ("info_link " + options[:class]).strip
-    link_to "Click for more information", path, {:title => 'Click for more information'}.merge(options)
-  end
 end
