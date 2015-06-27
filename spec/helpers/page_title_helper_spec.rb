b641e20842c9373390c4e988f66b5ecbd98b7ba1
Andrew White <andyw@pixeltrix.co.uk>
2015-06-27 18:27:05 +0100

    Use locale for page titles
    Having page titles generated using content_for makes Russian Doll caching
    tricky because when the fragment is cached the title is not generated. If
    we use the I18n features of Rails to generate page titles it works around
    the problem and makes the website easier to translate/update.

---
+++ spec/helpers/page_title_helper_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe PageTitleHelper, type: :helper do
+  before do
+    I18n.backend.store_translations(:fr, translations)
+  end
+
+  around do |example|
+    begin
+      locale, I18n.locale = I18n.locale, :fr
+      example.run
+    ensure
+      I18n.locale = locale
+    end
+  end
+
+  let :translations do
+    { page_titles: {
+        default: "Une pétition au Parlement",
+        static_pages: {
+          home: "Une pétition au Parlement - vers le bas avec ce genre de chose"
+        },
+        local_petitions: {
+          index: "Pétitions à %{constituency}"
+        },
+        petitions: {
+          default: "Voir toutes les pétitions",
+          show: "%{petition} - Pétitions"
+    }}}
+  end
+
+  describe "#page_title" do
+    context "when the controller and action keys exist" do
+      before do
+        params[:controller] = "static_pages"
+        params[:action] = "home"
+      end
+
+      it "uses the key 'page_titles.controller.action'" do
+        expect(helper.page_title).to eq("Une pétition au Parlement - vers le bas avec ce genre de chose")
+      end
+    end
+
+    context "when the action key doesn't exist" do
+      before do
+        params[:controller] = "petitions"
+        params[:action] = "unknown"
+      end
+
+      it "uses the key 'page_titles.controller.default'" do
+        expect(helper.page_title).to eq("Voir toutes les pétitions")
+      end
+    end
+
+    context "when the controller and action keys don't exist" do
+      before do
+        params[:controller] = "unknown"
+        params[:action] = "unknown"
+      end
+
+      it "uses the key 'page_titles.default'" do
+        expect(helper.page_title).to eq("Une pétition au Parlement")
+      end
+    end
+
+    context "when there is a petition assigned" do
+      let(:petition) { double(:petition, action: "Ban devoirs pour les enfants de l'école primaire") }
+
+      before do
+        params[:controller] = "petitions"
+        params[:action] = "show"
+        assign("petition", petition)
+      end
+
+      it "the petition action is available for interpolation" do
+        expect(helper.page_title).to eq("Ban devoirs pour les enfants de l'école primaire - Pétitions")
+      end
+    end
+
+    context "when there is a constituency assigned" do
+      let(:constituency) { double(:constituency, name: "Paris") }
+
+      before do
+        params[:controller] = "local_petitions"
+        params[:action] = "index"
+        assign('constituency', constituency)
+      end
+
+      it "the constituency name is available for interpolation" do
+        expect(helper.page_title).to eq("Pétitions à Paris")
+      end
+    end
+  end
+end
