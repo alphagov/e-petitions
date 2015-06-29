edab336bc087f6040ed2673107fb11b42490560c
Murray Steele <muz@h-lame.com>
2015-06-29 12:06:37 +0100

    Avoid passing `@query` around everywhere in searches_controller

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,18 +1,21 @@
 class Admin::SearchesController < Admin::AdminController
 
   def show
-    @query = params[:q]
-    if is_number?(@query)
-      find_petition_by_id(@query)
-    elsif is_email?(@query)
-      find_signatures_by_email(@query)
+    if query_is_number?
+      find_petition_by_id
+    elsif query_is_email?
+      find_signatures_by_email
     else
-      find_petitions_by_keyword(@query)
+      find_petitions_by_keyword
     end
   end
 
   private
-  def find_petition_by_id(query)
+  def query
+    @query ||= params.fetch(:q, '')
+  end
+
+  def find_petition_by_id
     begin
       petition = Petition.find(query.to_i)
       redirect_to admin_petition_url(petition)
@@ -22,19 +25,19 @@ class Admin::SearchesController < Admin::AdminController
     end
   end
 
-  def find_signatures_by_email(query)
-    @signatures = Signature.for_email(query).paginate(:page => params[:page], :per_page => 50)
+  def find_signatures_by_email
+    @signatures = Signature.for_email(query).paginate(page: params[:page], per_page: 50)
   end
 
-  def find_petitions_by_keyword(query)
+  def find_petitions_by_keyword
     redirect_to(controller: 'admin/petitions', action: 'index', q: query)
   end
 
-  def is_number?(query)
+  def query_is_number?
     /^\d+$/ =~ query
   end
 
-  def is_email?(query)
+  def query_is_email?
     query.include?('@')
   end
 end
