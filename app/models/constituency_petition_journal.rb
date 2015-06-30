97b9839fd498c26e93792857dcaaecf92703d7c1
Murray Steele <muz@h-lame.com>
2015-06-30 13:17:40 +0100

    Avoid extra queries in ``record_new_signature_for``
    Change to `find_or_intialize_by` in `.for` and let `#record_new_signature_for` handle the `new_record?` vs. `persisted?` cases differently.  For persisted records we do what we always did (an atomic update and reload), for new records we save the instance with a signature count of 1.
    
    This means we only do an INSERT for the "first signer of the petition from a constituency" rather than an INSERT (create the row), UPDATE (set the signature count), SELECT (reload the instance).

--- app/models/constituency_petition_journal.rb
+++ app/models/constituency_petition_journal.rb
@@ -15,7 +15,7 @@ class ConstituencyPetitionJournal < ActiveRecord::Base
   }
 
   def self.for(petition, constituency_id)
-    find_or_create_by(petition: petition, constituency_id: constituency_id)
+    find_or_initialize_by(petition: petition, constituency_id: constituency_id)
   end
 
   def self.record_new_signature_for(signature)
@@ -24,9 +24,13 @@ class ConstituencyPetitionJournal < ActiveRecord::Base
   end
 
   def record_new_signature(at = Time.current)
-    signature_count_field = self.class.connection.quote_column_name('signature_count')
-    changes = "#{signature_count_field} = #{signature_count_field} + 1, updated_at = :updated_at"
-    self.class.where(id: id).update_all([changes, updated_at: at])
-    reload
+    if self.new_record?
+      update_attributes(signature_count: 1)
+    else
+      signature_count_field = self.class.connection.quote_column_name('signature_count')
+      changes = "#{signature_count_field} = #{signature_count_field} + 1, updated_at = :updated_at"
+      self.class.unscoped.where(id: id).update_all([changes, updated_at: at])
+      reload
+    end
   end
 end
