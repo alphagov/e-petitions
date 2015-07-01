dff484ffe71824792aa1a798eebc3a3a493fe525
Pete Herlihy <peter.herlihy@digital.cabinet-office.gov.uk>
2015-07-01 16:11:19 +0100

    Updating error messages on create flow

--- app/models/staged/validations/petition_details.rb
+++ app/models/staged/validations/petition_details.rb
@@ -5,10 +5,10 @@ module Staged
 
       included do
         validates :action,
-          presence: { message: 'Action must be completed.' },
+          presence: { message: 'Action must be completed' },
           length: { maximum: 80, unless: ->(pd) { pd.action.blank? }, message: 'Action is too long' }
         validates :background,
-          presence: { message: 'Background must be completed.' },
+          presence: { message: 'Background must be completed' },
           length: { maximum: 300, unless: ->(pd) { pd.background.blank? }, message: 'Background is too long' }
         validates :additional_details,
           length: { maximum: 800, unless: ->(pd) { pd.additional_details.blank? }, message: 'Additional details is too long' }
