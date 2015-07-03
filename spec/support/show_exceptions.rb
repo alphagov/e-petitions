e8a3c0ab80d6b814fbcff0c8b582e5cfedc62650
Andrew White <andyw@pixeltrix.co.uk>
2015-07-03 22:07:59 +0100

    Add metadata filter for show_exceptions config
    By default we bubble up exceptions so that the error is raised in the
    spec however we need to be able to disable that to make sure that an
    error page gets rendered.

---
+++ spec/support/show_exceptions.rb
@@ -0,0 +1,27 @@
+module ShowExceptionsHelper
+  def with_show_exceptions(on_or_off, &block)
+    begin
+      env_config = Rails.application.env_config
+      show_exceptions = env_config['action_dispatch.show_exceptions']
+      env_config['action_dispatch.show_exceptions'] = on_or_off
+
+      yield
+    ensure
+      env_config['action_dispatch.show_exceptions'] = show_exceptions
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include(ShowExceptionsHelper, type: :request)
+
+  config.around(:each, type: :request) do |example|
+    if example.metadata.key?(:show_exceptions)
+      with_show_exceptions(example.metadata[:show_exceptions]) do
+        example.run
+      end
+    else
+      example.run
+    end
+  end
+end
