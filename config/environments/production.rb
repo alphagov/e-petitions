3583b44e8e0fd582649762236c6ef715f1066c29
Andrew White <andyw@pixeltrix.co.uk>
2015-07-05 18:15:55 +0100

    Add subdomains to the HSTS headers
    Rails doesn't include subdomains by default when sending the HSTS header
    as it doesn't know whether this is true but we do so force it to be true.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  # Set the HSTS headers to include subdomains
+  config.ssl_options[:hsts] = { expires: 365.days, subdomains: true }
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :info
