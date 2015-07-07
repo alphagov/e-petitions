13bb8d315e795e6e70748a2525ff7a6d0b906478
Henry Hadlow <henry@manofscience.co.uk>
2015-07-07 11:31:38 +0100

    [ci skip] Change wording on 'Yes, I am a British citizen...'
    Remove 'Yes, ' because there's not a corresponding question 'Are you a...?'
    Tweak spacing on `block-label` element

--- features/step_definitions/signature_steps.rb
+++ features/step_definitions/signature_steps.rb
@@ -38,7 +38,7 @@ end
 
 When /^I fill in my non\-UK details$/ do
   step "I fill in my details"
-  uncheck "Yes, I am a British citizen or UK resident"
+  uncheck "I am a British citizen or UK resident"
 end
 
 When(/^I fill in my details(?: with email "([^"]+)")?$/) do |email_address|
@@ -46,7 +46,7 @@ When(/^I fill in my details(?: with email "([^"]+)")?$/) do |email_address|
   steps %Q(
     When I fill in "Name" with "Womboid Wibbledon"
     And I fill in "Email" with "#{email_address}"
-    And I check "Yes, I am a British citizen or UK resident"
+    And I check "I am a British citizen or UK resident"
     And I fill in my postcode with "SW14 9RQ"
     And I select "United Kingdom" from "Location"
   )
@@ -56,7 +56,7 @@ When(/^I fill in my details with postcode "(.*?)"?$/) do |postcode|
   steps %Q(
     When I fill in "Name" with "Womboid Wibbledon"
     And I fill in "Email" with "womboid@wimbledon.com"
-    And I check "Yes, I am a British citizen or UK resident"
+    And I check "I am a British citizen or UK resident"
     And I fill in my postcode with "#{postcode}"
     And I select "United Kingdom" from "Location"
   )
