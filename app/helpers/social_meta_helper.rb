334380c6269bfbbd32770bf3c55fe29fadb9969f
Andrew White <andyw@pixeltrix.co.uk>
2015-07-08 16:48:03 +0100

    Fix social sharing metadata tags
    - Removed Dublin Core as they don't seem widely supported or used
    - Add generic OpenGraph and Twitter Card details
    - Add specific OpenGraph and Twitter Card details on petition pages

---
+++ app/helpers/social_meta_helper.rb
@@ -0,0 +1,21 @@
+module SocialMetaHelper
+  def open_graph_tag(name, content, interpolation = {})
+    if Symbol === content
+      tag(:meta, property: "og:#{name}", content: t(content, interpolation.merge(scope: :'metadata.open_graph')))
+    elsif name == 'image'
+      tag(:meta, property: "og:image", content: path_to_image(content))
+    else
+      tag(:meta, property: "og:#{name}", content: content)
+    end
+  end
+
+  def twitter_card_tag(name, content, interpolation = {})
+    if Symbol === content
+      tag(:meta, name: "twitter:#{name}", content: t(content, interpolation.merge(scope: :'metadata.twitter')))
+    elsif name == 'image'
+      tag(:meta, name: "twitter:image", content: path_to_image(content))
+    else
+      tag(:meta, name: "twitter:#{name}", content: content)
+    end
+  end
+end
