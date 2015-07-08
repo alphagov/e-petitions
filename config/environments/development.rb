0b36ed55906136377aba33373d869757c37db9d6
Andrew White <andyw@pixeltrix.co.uk>
2015-07-08 10:48:52 +0100

    Use :file delivery method in development
    Since Delayed Job can end up processing a lot of jobs at the same time it
    can be hard to track down the email you sent in development mode. By using
    the :file delivery method you can browse to the tmp/mails directory to
    look at the file instead of search through terminal history or a log file.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
+  # Tell Action Mailer not to deliver emails to the real world.
+  config.action_mailer.delivery_method = :file
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
