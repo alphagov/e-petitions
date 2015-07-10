c4a272ed9916e7468e74020c25fa715b70706619
Henry Hadlow <henry@manofscience.co.uk>
2015-07-10 17:09:09 +0100

    Fix bug where open graph and Twitter cards expect an image URL
    not a path (even though they figure it out ;)

--- app/helpers/social_meta_helper.rb
+++ app/helpers/social_meta_helper.rb
@@ -3,7 +3,7 @@ module SocialMetaHelper
     if Symbol === content
       tag(:meta, property: "og:#{name}", content: t(content, interpolation.merge(scope: :'metadata.open_graph')))
     elsif name == 'image'
-      tag(:meta, property: "og:image", content: path_to_image(content))
+      tag(:meta, property: "og:image", content: url_to_image(content))
     else
       tag(:meta, property: "og:#{name}", content: content)
     end
@@ -13,7 +13,7 @@ module SocialMetaHelper
     if Symbol === content
       tag(:meta, name: "twitter:#{name}", content: t(content, interpolation.merge(scope: :'metadata.twitter')))
     elsif name == 'image'
-      tag(:meta, name: "twitter:image", content: path_to_image(content))
+      tag(:meta, name: "twitter:image", content: url_to_image(content))
     else
       tag(:meta, name: "twitter:#{name}", content: content)
     end
