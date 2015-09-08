3d39ac79c6ecdb951fd920732985be453315098c
Andrew White <andrew.white@unboxedconsulting.com>
2015-09-08 20:31:40 +0100

    Fix Appsignal when used with multiple DJ workers
    Similar to the fix for Puma we need to let Appsignal know that the process
    has been forked so that it can re-establish connections. Unfortunately there
    is no good way to do this so we have to override the after_fork class method
    in an initializer to call Active Record's establish_connection as well as
    the Appsignal forked! method.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -20,3 +20,22 @@ module ActiveJob
     end
   end
 end
+
+# Override the default after_fork method on Delayed::Backend::ActiveRecord::Job
+# to restart the Appsignal agent when the worker is forked.
+module Delayed
+  module Backend
+    module ActiveRecord
+      class Job < ::ActiveRecord::Base
+        def self.after_fork
+          # Worker specific setup for Rails 4.1+
+          # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+          ::ActiveRecord::Base.establish_connection
+
+          # Let AppSignal know that a worker process has been forked
+          ::Appsignal.agent.forked! if defined?(::Appsignal) && ::Appsignal.config.active?
+        end
+      end
+    end
+  end
+end
