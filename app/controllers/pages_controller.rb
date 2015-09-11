17807da3ed1e1dee95d0a736f55c1eb7e6baa2f7
Andrew White <andrew.white@unboxedconsulting.com>
2015-09-11 12:23:46 +0100

    Add respond_to and respond_with to prevent template errors
    Calling a url like /help.txt raises a template error which we don't want
    to ignore in Appsignal so add mime responders to change those errors into
    unknown format errors which are already ignored.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,25 @@
 class PagesController < ApplicationController
+  respond_to :html, only: [:index, :help, :privacy]
+  respond_to :xml,  only: [:browserconfig]
+  respond_to :json, only: [:manifest]
+
   def index
+    respond_with(nil)
   end
 
   def help
+    respond_with(nil)
   end
 
   def privacy
+    respond_with(nil)
   end
 
   def browserconfig
+    respond_with(nil)
   end
 
   def manifest
+    respond_with(nil)
   end
 end
