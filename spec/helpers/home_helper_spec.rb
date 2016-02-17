13d6e32548c9fe162aa9ab65d3747d0eb9c9f1cf
Andrew White <andrew.white@unboxedconsulting.com>
2016-02-17 08:13:33 +0000

    Don't load all petitions on the home page
    The none? method on relations isn't optimised until Rails 5.0 so
    use the alternative method empty? which is optimised.

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -1,6 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe HomeHelper, type: :helper do
+  describe "#no_petitions_yet?" do
+    let(:connection) { Petition.connection }
+    let(:sql) { /^SELECT COUNT/ }
+
+    it "performs a count query" do
+      expect(connection).to receive(:select).with(sql, any_args).and_call_original
+      expect(helper.no_petitions_yet?).to be true
+    end
+
+    it "it caches the result" do
+      expect(connection).to receive(:select).once.with(sql, any_args).and_call_original
+      expect(helper.no_petitions_yet?).to be true
+      expect(helper.no_petitions_yet?).to be true
+    end
+
+    context "when there are no published petitions" do
+      before do
+        FactoryGirl.create(:pending_petition)
+      end
+
+      it "returns true" do
+        expect(helper.no_petitions_yet?).to be true
+      end
+    end
+
+    Petition::VISIBLE_STATES.each do |state|
+      context "when there is a #{state} petition" do
+        before do
+          FactoryGirl.create(:"#{state}_petition")
+        end
+
+        it "returns false" do
+          expect(helper.no_petitions_yet?).to be false
+        end
+      end
+    end
+  end
+
   describe "#petition_count" do
     describe "for counting government responses" do
       it "returns a HTML-safe string" do
