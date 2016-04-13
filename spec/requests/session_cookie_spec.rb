73af0d3a5474798f2d5c859b7f47aaa3bfa0590c
Andrew White <andrew.white@unboxedconsulting.com>
2016-04-13 06:40:39 +0100

    Add test for #453 session cookie expiry date change
    This is somewhat akin to testing Rails functionality but it's here so
    that it doesn't get changed back again as it's not the Rails default.

---
+++ spec/requests/session_cookie_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'session cookie', type: :request, show_exceptions: true do
+  let(:date) { Time.utc(2016, 4, 12, 12, 59, 59) }
+  let(:cookies) { response.header["Set-Cookie"].split("\n") }
+
+  subject do
+    cookies.find{ |c| c =~ /^_epets_session/ }
+  end
+
+  before do
+    petition = FactoryGirl.create(:open_petition)
+    get "/petitions/#{petition.id}/signatures/new"
+  end
+
+  around do |example|
+    travel_to(date) { example.run }
+  end
+
+  it "should set the secure option" do
+    expect(subject).to match(/; secure/i)
+  end
+
+  it "should set the httponly option" do
+    expect(subject).to match(/; httponly/i)
+  end
+
+  it "should set the expiry to 2 weeks from now" do
+    expect(subject).to match(/; expires=Tue, 26 Apr 2016 12:59:59 -0000/)
+  end
+end
