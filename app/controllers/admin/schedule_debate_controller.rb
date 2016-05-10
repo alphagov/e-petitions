0b6c287a61ccbefb67c60733f4474c00fe62003d
Andrew White <andrew.white@unboxed.co>
2016-05-10 18:54:57 +0100

    Remove email button from admin screen
    If there's no email to send then we don't need a button.

--- app/controllers/admin/schedule_debate_controller.rb
+++ app/controllers/admin/schedule_debate_controller.rb
@@ -8,13 +8,7 @@ class Admin::ScheduleDebateController < Admin::AdminController
 
   def update
     if @petition.update_attributes(params_for_update)
-      if send_email_to_petitioners?
-        EmailDebateScheduledJob.run_later_tonight(petition: @petition)
-        message = 'Email will be sent overnight'
-      else
-        message = 'Updated the scheduled debate date successfully'
-      end
-
+      message = 'Updated the scheduled debate date successfully'
       redirect_to [:admin, @petition], notice: message
     else
       render 'admin/petitions/show'
@@ -29,9 +23,5 @@ class Admin::ScheduleDebateController < Admin::AdminController
 
   def params_for_update
     params.require(:petition).permit(:scheduled_debate_date)
-  end
-
-  def send_email_to_petitioners?
-    params.key?(:save_and_email)
   end
 end
