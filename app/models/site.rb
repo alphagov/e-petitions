701eb12aef2486d3fb22d13605236db5d38f2dc4
Andrew White <andrew.white@unboxed.co>
2016-06-24 18:22:06 +0100

    Remove trending petitions for the moment
    The trending petitions query is expensive to calculate and even though
    the section is fragment cached the rate of updates is effectively giving
    a hit rate of zero.

--- app/models/site.rb
+++ app/models/site.rb
@@ -84,6 +84,10 @@ class Site < ActiveRecord::Base
       instance.closed_at_for_opening(time)
     end
 
+    def show_trending_petitions?
+      false
+    end
+
     def signature_count_updated_since_window
       5.minutes
     end
