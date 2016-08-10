a4d0061faf5b71ad0653c2b7b572f5e39dcd37c3
Andrew White <andrew.white@unboxed.co>
2016-08-10 18:06:35 +0100

    Add request specs for the archived petitions API

---
+++ spec/requests/archived_petitions_list_spec.rb
@@ -0,0 +1,129 @@
+require 'rails_helper'
+require_relative 'api_request_helpers'
+
+RSpec.describe 'API request to list archived petitions', type: :request, show_exceptions: true do
+  include ApiRequestHelpers
+
+  def make_successful_request(params = {})
+    get archived_petitions_url({format: 'json'}.merge(params))
+    expect(response).to be_success
+  end
+
+  describe "format" do
+    it "responds to JSON" do
+      make_successful_request
+    end
+
+    it "does not respond to XML" do
+      get archived_petitions_url(format: 'xml')
+      expect(response.status).to eq(406)
+    end
+  end
+
+  describe "links" do
+    before do
+      FactoryGirl.create_list :archived_petition, 3
+    end
+
+    it "returns a link to itself" do
+      make_successful_request
+
+      expect(json["links"]).to include({"self" => archived_petitions_url(format: 'json')})
+    end
+
+    it "returns a link to the first page of results" do
+      make_successful_request count: 2
+
+      expect(json["links"]).to include({"first" => archived_petitions_url(count: 2, format: 'json')})
+    end
+
+    it "returns a link to the last page of results" do
+      make_successful_request count: 2
+
+      expect(json["links"]).to include({"last" => archived_petitions_url(count: 2, page: 2, format: 'json')})
+    end
+
+    it "returns a link to the next page of results if there is one" do
+      make_successful_request count: 2
+
+      expect(json["links"]).to include({"next" => archived_petitions_url(count: 2 ,page: 2, format: 'json')})
+    end
+
+    it "returns a link to the previous page of results if there is one" do
+      make_successful_request count: 2, page: 2
+
+      expect(json["links"]).to include({"prev" => archived_petitions_url(count: 2, format: 'json')})
+    end
+
+    it "returns no link to the previous page of results when on the first page of results" do
+      make_successful_request count: 2
+
+      expect(json["links"]).to include({"prev" => nil})
+    end
+
+    it "returns no link to the next page of results when on the last page of results" do
+      make_successful_request count: 2, page: 2
+
+      expect(json["links"]).to include({"next" => nil})
+    end
+
+    it "returns the last link == first link for empty results" do
+      make_successful_request count: 2, page: 2, state: 'rejected'
+
+      expect(json["links"]).to include({"last" => json["links"]["first"]})
+    end
+
+    it "returns previous page link == last link when paging off the end of the results" do
+      make_successful_request count: 2, page: 3, state: 'rejected'
+
+      expect(json["links"]).to include({"prev" => json["links"]["last"]})
+    end
+  end
+
+  describe "data" do
+    it "returns an empty response if no petitions are public" do
+      make_successful_request
+
+      expect(json["data"]).to be_empty
+    end
+
+    it "returns a list of serialized petitions in the expected order" do
+      petition_1 = FactoryGirl.create :archived_petition, signature_count: 100
+      petition_2 = FactoryGirl.create :archived_petition, signature_count: 300
+      petition_3 = FactoryGirl.create :archived_petition, signature_count: 200
+
+      make_successful_request
+
+      expect(json["data"].length).to eq(3)
+
+      expect(json["data"][0]["attributes"]["title"]).to eq(petition_2.title)
+      expect(json["data"][1]["attributes"]["title"]).to eq(petition_3.title)
+      expect(json["data"][2]["attributes"]["title"]).to eq(petition_1.title)
+    end
+
+    it "includes a link to each petitions details" do
+      petition = FactoryGirl.create :archived_petition
+
+      make_successful_request
+
+      expect(json["data"][0]["links"]).to be_a Hash
+      expect(json["data"][0]["links"]).to include("self" => archived_petition_url(petition, format: 'json'))
+    end
+
+    it "includes the rejection section for rejected petitions" do
+      petition = FactoryGirl.create :archived_petition, :rejected
+
+      make_successful_request
+
+      expect(json["data"][0]["attributes"]["rejection"]["details"]).to eq(petition.reason_for_rejection)
+    end
+
+    it "includes the government_response section for petitions with a government_response" do
+      petition = FactoryGirl.create :archived_petition, :response
+
+      make_successful_request
+
+      expect(json["data"][0]["attributes"]["government_response"]["details"]).to eq(petition.response)
+    end
+  end
+end
