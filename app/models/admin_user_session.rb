4f5586d4d9554d432bb9adab6249a7f76f43efdf
Andrew White <andrew.white@unboxed.co>
2016-10-05 16:01:32 +0100

    Reset persistence token on login/logout
    To minimise the possibility of an admin user credentials cookie being
    replayed after a MITM SSL attack reset them on login/logout.

--- app/models/admin_user_session.rb
+++ app/models/admin_user_session.rb
@@ -2,6 +2,14 @@ class AdminUserSession < Authlogic::Session::Base
   allow_http_basic_auth false
   consecutive_failed_logins_limit AdminUser::DISABLED_LOGIN_COUNT
 
+  before_save do
+    record.reset_persistence_token!
+  end
+
+  before_destroy do
+    record.reset_persistence_token!
+  end
+
   def last_login_attempt?
     failed_login_count == consecutive_failed_logins_limit - 1
   end
