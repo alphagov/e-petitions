f097828ff69b78414ffe752e9321763d816bf148
AbraaoMota <abraaomot@outlook.com>
2017-04-20 12:51:34 +0100

    Make pagination helper return html
    The `paginate` helper method was appending its output through a side
    effect of concat. It now returns its html, and the view was updated
    accordingly.

--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -1,6 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe SearchHelper, type: :helper do
+
+  describe "#paginate" do
+
+    let (:page_params) {{
+      :total_pages=>3,
+      :previous_link=>"/petitions?page=1&state=all",
+      :next_link=>"/petitions?state=all",
+      "model" => Petition
+    }}
+
+    let (:first_page_params) {{
+      "first_page?" => true,
+      "last_page?" => false,
+      "previous_params" => {:state=>:all, :page=>nil},
+      "next_params" => {:state=>:all, :page=>2},
+      :previous_page=>nil,
+      :next_page=>2,
+    }}
+
+    let (:intermediary_page_params) {{
+      "first_page?" => false,
+      "last_page?" => false,
+      "previous_params" => {:state=>:all, :page=>1},
+      "next_params" => {:state=>:all, :page=>3},
+      :previous_page=>1,
+      :next_page=>2,
+    }}
+
+    let (:last_page_params) {{
+      "first_page?" => false,
+      "last_page?" => true,
+      "previous_params" => {:state=>:all, :page=>2},
+      "next_params" => {:state=>:all, :page=>nil},
+      :previous_page=>2,
+      :next_page=>nil,
+    }}
+
+    context "when it handles the first page of petitions" do
+      it "only adds a link to the next page" do
+        petitions = double('petitions', page_params.merge(first_page_params))
+        expect(paginate(petitions)).to include "Next"
+        expect(paginate(petitions)).not_to include "Previous"
+      end
+    end
+
+    context "when it handles an intermediary page of petitions" do
+      it "adds both pagination links" do
+        petitions = double('petitions', page_params.merge(intermediary_page_params))
+        expect(paginate(petitions)).to include "Next"
+        expect(paginate(petitions)).to include "Previous"
+      end
+    end
+
+    context "when it handles the last page of petitions" do
+      it "only adds a link to the previous page" do
+        petitions = double('petitions', page_params.merge(last_page_params))
+        expect(paginate(petitions)).to include "Previous"
+        expect(paginate(petitions)).not_to include "Next"
+      end
+    end
+  end
+
   describe "#filtered_petition_count" do
     context 'when search term is not present' do
       it 'renders correctly with > 1 results' do
