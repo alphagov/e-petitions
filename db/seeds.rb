2930feb422a0663700c20f2966b96992caf90809
James Cook <jamesmarkcook@gmail.com>
2017-06-05 10:32:03 +0100

    Setup petitions and signatures in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+require './spec/factories'
+
+if Rails.env.development?
+  unless AdminUser.where(role: "sysadmin").count > 0
+    FactoryGirl.create(:sysadmin_user)
+  end
+
+  unless AdminUser.where(role: "moderator").count > 0
+    FactoryGirl.create(:moderator_user)
+  end
+
+  # Create some petitions
+  [:open_petition, :closed_petition, :rejected_petition].each do |petition_type|
+    FactoryGirl.create_list(petition_type, 100)
+  end
+
+  # Create some signatures
+  first_id, last_id = Petition.first.id, Petition.last.id
+
+  25.times do
+    rand(first_id..last_id).tap do |petition_id|
+      FactoryGirl.create_list(:validated_signature, rand(5..10), petition_id: petition_id)
+    end
+  end
+end
