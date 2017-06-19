01074c74c63ac23478d5d54717da633b3dfc9a9a
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2017-06-19 12:17:28 +0100

    Remove unnecessary argument in mailers 'from' lambdas
    This was causing an argument error which I don't fully grasp but seems to
    be related to new actionmailer paramerisation.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default_url_options[:protocol] = Site.email_protocol
-  default from: ->(email){ Site.email_from }
+  default from: -> { Site.email_from }
 
   helper :date_time, :rejection
 
