f2db40f1d242d283c0e9ef3eb3895daa17604bc1
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2017-06-28 10:16:44 +0100

    Show split moderation counts on admin dashboard
    Also:
      - Remove the 'All petitions' panel and show a regular text link at the
      bottom of the page instead.

--- spec/helpers/admin_hub_helper_spec.rb
+++ spec/helpers/admin_hub_helper_spec.rb
@@ -1,6 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe HomeHelper, type: :helper do
+RSpec.describe AdminHubHelper, type: :helper do
+  describe "moderation count helpers" do
+    before do
+      allow(Admin::Settings).to receive(:first).and_return(Admin::Settings.create(petition_tags: "one"))
+    end
+
+    let!(:petition_recently_in_moderation) { FactoryGirl.create(:recently_in_moderation_petition) }
+    let!(:petition_nearly_overdue_moderation) { FactoryGirl.create(:nearly_overdue_moderation_petition) }
+    let!(:petition_overdue_moderation) { FactoryGirl.create(:overdue_moderation_petition) }
+    let!(:tagged_in_moderation_petition) { FactoryGirl.create(:validated_petition, tags: ['one']) }
+    let!(:sponsored_petition) { FactoryGirl.create(:sponsored_petition, tags: ['one']) }
+
+    describe "in_moderation_count" do
+      it 'returns the number in moderation' do
+        expect(helper.in_moderation_count).to eq 4
+      end
+    end
+
+    describe "recently_in_moderation_count" do
+      it 'returns the number recently in moderation' do
+        expect(helper.recently_in_moderation_count).to eq 1
+      end
+    end
+
+    describe "nearly_overdue_moderation_count" do
+      it 'returns the number nearly overdue moderation' do
+        expect(helper.nearly_overdue_moderation_count).to eq 1
+      end
+    end
+
+    describe "overdue_moderation_count" do
+      it 'returns the number overdue moderation' do
+        expect(helper.overdue_moderation_count).to eq 1
+      end
+    end
+
+    describe "tagged_count" do
+      it 'returns the number of tagged petitions' do
+        expect(helper.tagged_count).to eq 1
+      end
+    end
+  end
+
   describe "#action_count" do
     describe "for counting the moderation queue" do
       it "returns a HTML-safe string" do
