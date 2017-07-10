1aabac3dedb8346ab6cd03da369eac0732826697
Andrew White <andrew.white@unboxed.co>
2017-07-10 16:08:08 +0100

    Wrap find_each blocks in without_instrumentation
    Appsignal will try to store all of the queries performed by
    find_each to send to the monitoring API but this leads to
    significant memory growth.

--- app/jobs/archive_petitions_job.rb
+++ app/jobs/archive_petitions_job.rb
@@ -2,11 +2,13 @@ class ArchivePetitionsJob < ApplicationJob
   queue_as :high_priority
 
   def perform
-    Petition.find_each do |petition|
-      next if petition.archived?
+    Appsignal.without_instrumentation do
+      Petition.find_each do |petition|
+        next if petition.archived?
 
-      ArchivePetitionJob.perform_later(petition)
-      petition.update_column(:archiving_started_at, Time.current)
+        ArchivePetitionJob.perform_later(petition)
+        petition.update_column(:archiving_started_at, Time.current)
+      end
     end
   end
 end
