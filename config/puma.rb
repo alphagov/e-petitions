7863731fe80307cd3a7a8ec5cbc9f88522bff491
Andrew White <andrew.white@unboxed.co>
2020-03-01 11:40:29 +0000

    Update deployment config for new infrastructure

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,22 @@
-if ENV['RAILS_ENV'] == 'production'
-  # Pumacorn specific items:
-  application_name = "epetitions"
-  pidfile "/home/deploy/#{application_name}/shared/pids/puma.pid"
-  bind "unix:///var/run/pumacorn/#{application_name}.sock"
+if ENV["RAILS_ENV"] == "production"
+  pidfile "#{File.expand_path('../../tmp/pids/puma.pid', __FILE__)}"
+  bind "unix://#{File.expand_path('../../tmp/sockets/puma.sock', __FILE__)}"
 
-  # Based on https://raw.githubusercontent.com/codetriage/codetriage/master/config/puma.rb
   concurrency = {
-    # Set to default of 4 workers - c4.xlarge has 4 CPUs
-    workers: ENV.fetch('WEB_CONCURRENCY') { 4 }.to_i,
-    # Some experimentation seems to indicate these are reasonable options:
-    min_threads: ENV.fetch('WEB_CONCURRENCY_MIN_THREADS') { 16 }.to_i,
-    max_threads: ENV.fetch('WEB_CONCURRENCY_MAX_THREADS') { 32 }.to_i
+    workers: ENV.fetch("WEB_CONCURRENCY") { 4 }.to_i,
+    min_threads: ENV.fetch("WEB_CONCURRENCY_MIN_THREADS") { 8 }.to_i,
+    max_threads: ENV.fetch("WEB_CONCURRENCY_MAX_THREADS") { 32 }.to_i
   }
 
   workers(concurrency[:workers])
   threads(concurrency[:min_threads], concurrency[:max_threads])
-
   preload_app!
 
   on_worker_boot do
     # Reset the connection to the cache if we can
-    Rails.cache.reset if Rails.cache.respond_to?(:reset)
+    ::Rails.cache.reset if ::Rails.cache.respond_to?(:reset)
 
-    # Worker specific setup for Rails 4.1+
-    # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+    # Establish a connection to the database
     ActiveRecord::Base.establish_connection
   end
 end
