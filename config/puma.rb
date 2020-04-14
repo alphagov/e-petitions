359f7777a9f8ffae9bc6b4d2f95b92789094da76
Andrew White <andrew.white@unboxed.co>
2020-04-14 08:03:01 +0100

    Update puma gem to take advantage of magic dashboard

--- config/puma.rb
+++ config/puma.rb
@@ -1,36 +1,24 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+if ENV["RAILS_ENV"] == "production"
+  pidfile "#{File.expand_path('../../tmp/pids/puma.pid', __FILE__)}"
+  bind "unix://#{File.expand_path('../../tmp/sockets/puma.sock', __FILE__)}"
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+  concurrency = {
+    workers: ENV.fetch("WEB_CONCURRENCY") { 4 }.to_i,
+    min_threads: ENV.fetch("WEB_CONCURRENCY_MIN_THREADS") { 8 }.to_i,
+    max_threads: ENV.fetch("WEB_CONCURRENCY_MAX_THREADS") { 32 }.to_i
+  }
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+  workers(concurrency[:workers])
+  threads(concurrency[:min_threads], concurrency[:max_threads])
+  preload_app!
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-if ENV.key?("WEB_CONCURRENCY")
-  workers ENV.fetch("WEB_CONCURRENCY")
+  plugin :appsignal
+
+  on_worker_boot do
+    # Reset the connection to the cache if we can
+    ::Rails.cache.reset if ::Rails.cache.respond_to?(:reset)
+
+    # Establish a connection to the database
+    ActiveRecord::Base.establish_connection
+  end
 end
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-preload_app!
-#
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
