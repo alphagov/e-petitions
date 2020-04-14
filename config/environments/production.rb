84570aa679e468c089691ff18b89b15970c6f959
Andrew White <andrew.white@unboxed.co>
2020-04-14 08:03:37 +0100

    Disable email delivery in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,15 +90,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: ENV.fetch('SMTP_HOSTNAME'),
-    port: ENV.fetch('SMTP_PORT'),
-    user_name: ENV.fetch('SMTP_USERNAME'),
-    password: ENV.fetch('SMTP_PASSWORD'),
-    authentication: :login,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.perform_deliveries = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
