498d88d62265de40abcc6e79b574a1a15a2595df
Andrew White <andrew.white@unboxed.co>
2020-04-22 07:56:45 +0100

    Ensure sharing links aren't broken by translation helper

--- app/helpers/sharing_helper.rb
+++ app/helpers/sharing_helper.rb
@@ -1,63 +1,30 @@
 module SharingHelper
   def share_via_facebook(petition, options = {})
-    link_to(share_button(:facebook), share_via_facebook_url(petition), options)
-  end
-
-  def share_via_facebook_url(petition)
-    "https://www.facebook.com/sharer/sharer.php?#{share_via_facebook_params(petition)}"
+    share_button(:facebook, share_params(petition))
   end
 
   def share_via_email(petition, options = {})
-    link_to(share_button(:email), share_via_email_url(petition), options)
-  end
-
-  def share_via_email_url(petition)
-    "mailto:?#{share_via_email_params(petition)}"
+    share_button(:email, share_params(petition))
   end
 
   def share_via_twitter(petition, options = {})
-    link_to(share_button(:twitter), share_via_twitter_url(petition), options)
-  end
-
-  def share_via_twitter_url(petition)
-    "https://twitter.com/intent/tweet?#{share_via_twitter_params(petition)}"
+    share_button(:twitter, share_params(petition))
   end
 
   def share_via_whatsapp(petition, options = {})
-    link_to(share_button(:whatsapp), share_via_whatsapp_url(petition), options)
-  end
-
-  def share_via_whatsapp_url(petition)
-    "whatsapp://send?#{share_via_whatsapp_params(petition)}"
+    share_button(:whatsapp, share_params(petition))
   end
 
   private
 
-  def share_via_facebook_params(petition)
-    share_params(u: petition_url(petition), ref: "responsive")
+  def share_params(petition)
+    {
+      url: URI.encode_www_form_component(petition_url(petition)),
+      action: URI.encode_www_form_component(petition.action).gsub('+', '%20')
+    }
   end
 
-  def share_via_email_params(petition)
-    share_params(subject: share_title(petition), body: petition_url(petition))
-  end
-
-  def share_via_twitter_params(petition)
-    share_params(text: share_title(petition), url: petition_url(petition))
-  end
-
-  def share_via_whatsapp_params(petition)
-    share_params(text: "#{share_title(petition)}\n#{petition_url(petition)}")
-  end
-
-  def share_title(petition)
-    t(:share_title, scope: :ui, petition: petition.action)
-  end
-
-  def share_params(hash)
-    hash.to_query.gsub('+', '%20')
-  end
-
-  def share_button(service)
-    t(:"#{service}_html", scope: :"ui.sharing")
+  def share_button(service, params)
+    t(:"#{service}_html", { scope: :"ui.sharing" }.merge(params))
   end
 end
--- spec/helpers/sharing_helper_spec.rb
+++ spec/helpers/sharing_helper_spec.rb
@@ -1,44 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe SharingHelper, type: :helper do
-  let(:petition) { double(:petition, to_param: "100000", action: "Do something") }
+  let(:petition) { FactoryBot.create(:open_petition, id: 100000, action_en: "Do Something!", action_cy: "Gwneud Rhywbeth!") }
   let(:headers) { helper.request.env }
 
   before do
-    headers["HTTP_HOST"]   = "petitions.senedd.wales"
     headers["HTTPS"]       = "on"
     headers["SERVER_PORT"] = 443
   end
 
-  describe "#share_via_facebook_url" do
-    it "generates a share via Facebook url" do
-      expect(helper.share_via_facebook_url(petition)).to eq <<-URL.strip
-        https://www.facebook.com/sharer/sharer.php?ref=responsive&u=https%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000
-      URL
+  context "when viewing the site in English" do
+    before do
+      headers["HTTP_HOST"]   = "petitions.senedd.wales"
+      I18n.locale = :"en-GB"
+    end
+
+    describe "#share_via_facebook" do
+      it "generates a share via Facebook link" do
+        expect(helper.share_via_facebook(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="https://www.facebook.com/sharer/sharer.php?ref=responsive&amp;u=https%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000">Facebook</a>
+        URL
+      end
+    end
+
+    describe "#share_via_email" do
+      it "generates a share via email link" do
+        expect(helper.share_via_email(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="mailto:?body=https%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000&amp;subject=Petition%3A%20Do%20Something%21">Email</a>
+        URL
+      end
+    end
+
+    describe "#share_via_twitter" do
+      it "generates a share via Twitter link" do
+        expect(helper.share_via_twitter(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="https://twitter.com/intent/tweet?text=Petition%3A%20Do%20Something%21&amp;url=https%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000">Twitter</a>
+        URL
+      end
+    end
+
+    describe "#share_via_whatsapp" do
+      it "generates a share via Whatsapp link" do
+        expect(helper.share_via_whatsapp(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="whatsapp://send?text=Petition%3A%20Do%20Something%21%0Ahttps%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000">Whatsapp</a>
+        URL
+      end
     end
   end
 
-  describe "#share_via_email_url" do
-    it "generates a share via email url" do
-      expect(helper.share_via_email_url(petition)).to eq <<-URL.strip
-        mailto:?body=https%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000&subject=Petition%3A%20Do%20something
-      URL
+  context "when viewing the site in Welsh" do
+    before do
+      headers["HTTP_HOST"]   = "deisebau.senedd.cymru"
+      I18n.locale = :"cy-GB"
     end
-  end
 
-  describe "#share_via_twitter_url" do
-    it "generates a share via Twitter url" do
-      expect(helper.share_via_twitter_url(petition)).to eq <<-URL.strip
-        https://twitter.com/intent/tweet?text=Petition%3A%20Do%20something&url=https%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000
-      URL
+    describe "#share_via_facebook" do
+      it "generates a share via Facebook link" do
+        expect(helper.share_via_facebook(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="https://www.facebook.com/sharer/sharer.php?ref=responsive&amp;u=https%3A%2F%2Fdeisebau.senedd.cymru%2Fdeisebau%2F100000">Facebook</a>
+        URL
+      end
     end
-  end
 
-  describe "#share_via_whatsapp_url" do
-    it "generates a share via Whatsapp url" do
-      expect(helper.share_via_whatsapp_url(petition)).to eq <<-URL.strip
-        whatsapp://send?text=Petition%3A%20Do%20something%0Ahttps%3A%2F%2Fpetitions.senedd.wales%2Fpetitions%2F100000
-      URL
+    describe "#share_via_email" do
+      it "generates a share via email link" do
+        expect(helper.share_via_email(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="mailto:?body=https%3A%2F%2Fdeisebau.senedd.cymru%2Fdeisebau%2F100000&amp;subject=Deiseb%3A%20Gwneud%20Rhywbeth%21">E-bost</a>
+        URL
+      end
+    end
+
+    describe "#share_via_twitter" do
+      it "generates a share via Twitter link" do
+        expect(helper.share_via_twitter(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="https://twitter.com/intent/tweet?text=Deiseb%3A%20Gwneud%20Rhywbeth%21&amp;url=https%3A%2F%2Fdeisebau.senedd.cymru%2Fdeisebau%2F100000">Twitter</a>
+        URL
+      end
+    end
+
+    describe "#share_via_whatsapp" do
+      it "generates a share via Whatsapp link" do
+        expect(helper.share_via_whatsapp(petition)).to eq <<-URL.strip
+          <a rel="external" target="_blank" href="whatsapp://send?text=Deiseb%3A%20Gwneud%20Rhywbeth%21%0Ahttps%3A%2F%2Fdeisebau.senedd.cymru%2Fdeisebau%2F100000">Whatsapp</a>
+        URL
+      end
     end
   end
 end
