89019b1d77964194f48159291d95eaf50effe782
Andrew White <andrew.white@unboxed.co>
2020-10-07 06:07:21 +0100

    Avoid loading unnecessary railties
    This is just Action Cable and Active Storage for now but will also
    prevent Action Text and Action Mailbox from being loaded when the app
    is upgraded to Rails 6.0.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,17 @@ require File.expand_path('../boot', __FILE__)
 require File.expand_path('../../lib/cloud_front_remote_ip', __FILE__)
 require File.expand_path('../../lib/quiet_logger', __FILE__)
 
-require 'rails/all'
+require 'rails'
+
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
