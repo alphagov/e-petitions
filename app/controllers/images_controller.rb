5cc24bb5e44f41b757268c08f4dccc3903b70b0d
Andrew White <andrew.white@unboxed.co>
2021-05-23 11:10:40 +0100

    Return 404 for invalid variation keys
    This will be fixed in 6.1.4 but override the method for now.
    
    Ref: rails/rails#41772

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,13 @@ class ImagesController < ActiveStorage::Representations::ProxyController
   def admin_request?
     false
   end
+
+  private
+    # TODO: Remove this when Rails 6.1.4 is released
+    # Reference: https://github.com/rails/rails/issues/41772
+    def set_representation
+      @representation = @blob.representation(params[:variation_key]).processed
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+      head :not_found
+    end
 end
