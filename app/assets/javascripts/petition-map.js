1d7cded9e6b1c18c98e06a9ec5d504ea18f9ace7
Despo Pentara <despo@extractmethod.com>
2022-03-10 09:44:18 +0000

    Basic petition map implementation

--- app/assets/javascripts/petition-map.js
+++ app/assets/javascripts/petition-map.js
@@ -1,117 +1,120 @@
-class PetitionMap {
-  constructor () {
-
-    this.extendTopoJson();
-    this.setupMap();
-
-    this.fetchPetitionData();
-
-    this.constituency_json = this.addConstituenciesToMap();
-    this.constituency_json.addTo(this.map);
-    this.addConstituencyData();
-  }
-
-  async fetchPetitionData() {
-    this.petition_data = JSON.parse(document.getElementById('data').textContent);
-  }
-
-  async getGeoData(url) {
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-  }
-
-  async fetchConstituencyData() {
-    let constituency_data = await this.getGeoData('../../welsh-constituencies.topojson');
-
-    return constituency_data;
-  }
-
-  async fetchRegionalData() {
-    let regional_data = await this.getGeoData('../../welsh-regions.topojson')
-
-    return regional_data;
-  }
-
-  addConstituenciesToMap() {
-    return this.topoJson(null, {
-      style: this.style,
-      //onEachFeature: onEachFeature,
-      minZoom: 0,
-      maxZoom: 0
-    })
-  }
-
-  async addConstituencyData() {
-    let data = await this.fetchConstituencyData();
-
-    await this.constituency_json.addData(data);
-  }
-
-  setupMap() {
-    this.map = L.map('map', {
-      zoom: 2,
-      zoomControl: false,
-      attributionControl: false,
-      trackResize: true,
-      minZoom: 6,
-      maxZoom: 9,
-      zoomAnimation: false
-    })
-
-    this.map.setView([52.4, -3.53976], 8);
-    return 'no';
-  }
-
-  topoJson(data, options) {
-    return new L.TopoJSON(data, options);
-  };
-
-  extendTopoJson() {
-    L.TopoJSON = L.GeoJSON.extend({
-      addData: function (data) {
-        var geojson, key;
-        if (data.type === "Topology") {
-          for (key in data.objects) {
-            if (data.objects.hasOwnProperty(key)) {
-              geojson = topojson.feature(data, data.objects[key]);
-              L.GeoJSON.prototype.addData.call(this, geojson);
-            }
-          }
-          return this;
+L.TopoJSON = L.GeoJSON.extend({
+  addData: function (data) {
+    var geojson, key;
+    if (data.type === "Topology") {
+      for (key in data.objects) {
+        if (data.objects.hasOwnProperty(key)) {
+          geojson = topojson.feature(data, data.objects[key]);
+          L.GeoJSON.prototype.addData.call(this, geojson);
         }
-        L.GeoJSON.prototype.addData.call(this, data);
-        return this;
       }
-    });
+      return this;
+    }
+    L.GeoJSON.prototype.addData.call(this, data);
+    return this;
   }
+});
 
-  style(feature) {
-    return {
-      color: "#777",
-      fillColor: '#111100',
-      opacity: 1,
-      weight: 1,
-      fillColor: '#999999',
-      fillOpacity: 0.6
-    };
-  }
+L.PetitionMap = {};
+L.PetitionMap.data = [];
+L.PetitionMap.colors = ['#FBEEF5',
+                        '#F1C2DD',
+                        '#EFB9D8',
+                        '#E799C6',
+                        '#E07BB5',
+                        '#D856A0',
+                        '#D85AA2',
+                        '#CB1F81',
+                        '#CC2685']
 
-  getColor(feature) {
-    var signaturePercentage = signaturePercentageByConstituency(feature.id);
+const setupMap = () =>  {
+  let map = L.map('map', {
+    zoom: 2,
+    zoomControl: true,
+    attributionControl: false,
+    trackResize: true,
+    minZoom: 6,
+    maxZoom: 9,
+    zoomAnimation: false
+  })
+  map.setView([52.4, -3.53976], 8);
 
-    var colors = ['#f189c5', '#eb5bae', '#e62e98', '##c9187e', '#9b1361', '#6e0d45', '#400828']
-    return colors[0];
-  }
-
-  signaturePercentageByConstituency(code) {
-    signatures = this.petition_data['signatures_by_constituency'];
-    signatures_for_constituency = signatures.filter(function(e){return e.id == code});
-    all_signatures = Object.values(signatures).reduce((a, b) => a + b.signature_count);
-    console.log(all_signatures);
-
-    return signatures_for_constituency.length > 0 ? signatures_for_constituency[0].signature_count : 0;
-  }
+  return map;
 }
 
-const petitionMap = new PetitionMap();
+const fetchPetitionData = () => {
+  L.PetitionMap.data = JSON.parse(document.getElementById('data').textContent);
+}
+
+const getColor = (feature) => {
+  let signaturePercentage = signaturePercentageByConstituency(feature.id);
+
+  return signaturePercentage > 30 ? L.PetitionMap.colors[7] :
+    signaturePercentage > 20  ? L.PetitionMap.colors[6] :
+    signaturePercentage > 5   ? L.PetitionMap.colors[5] :
+    signaturePercentage > 4   ? L.PetitionMap.colors[4] :
+    signaturePercentage > 3   ? L.PetitionMap.colors[3] :
+    signaturePercentage > 2   ? L.PetitionMap.colors[2] :
+    signaturePercentage > 1   ? L.PetitionMap.colors[1] :
+                                L.PetitionMap.colors[0];
+}
+
+const signaturePercentageByConstituency = (code) => {
+  let signatures = L.PetitionMap.data['data']['attributes']['signatures_by_constituency'] || [];
+  let total_signatures = L.PetitionMap.data['data']['attributes']['signature_count'] || 0;
+
+  let signatures_for_constituency = signatures.filter(function(e){return e.id == code});
+  let percentage = signatures_for_constituency.length > 0 ?
+                    signatures_for_constituency[0].signature_count*100 / total_signatures :
+                    1;
+
+  return percentage;
+}
+
+const fetchConstituencyData = async () => {
+  let data = await getGeoData('../../welsh-constituencies.topojson');
+
+  return data;
+}
+
+const getGeoData = async (url) => {
+  let response = await fetch(url);
+  let data = await response.json();
+
+  return data;
+}
+
+const addLayerToMap = (map) => {
+  return topoJson(null, {
+    style: style,
+    minZoom: 0,
+    maxZoom: 0
+  }).addTo(map);
+}
+
+const topoJson = (data, options) => {
+  return new L.TopoJSON(data, options);
+}
+
+const style = (feature) => {
+  return {
+    color: "#999",
+    opacity: 1,
+    weight: 1,
+    fillColor: getColor(feature),
+    fillOpacity: 0.6
+  };
+}
+
+const setup = async () => {
+  let map = setupMap();
+  fetchPetitionData();
+
+  let constituenciesData = await fetchConstituencyData();
+  let constituencyLayer = await addLayerToMap(map);
+
+  constituencyLayer.addData(constituenciesData);
+  L.control.layers({"constituencies": constituencyLayer}).addTo(map);
+}
+
+setup();
