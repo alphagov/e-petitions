a3ff39a6ef352c89c916ba4d02d44acd75a0182d
Andrew White <andrew.white@unboxed.co>
2024-12-21 20:01:32 +0000

    Tidy up parliaments API response and specs

--- app/views/parliaments/show.json.jbuilder
+++ app/views/parliaments/show.json.jbuilder
@@ -1,14 +1,16 @@
 json.period @parliament.period
 json.dissolution_at @parliament.dissolution_at
+json.government @parliament.government
+json.response_threshold @parliament.threshold_for_response
+json.debate_threshold @parliament.threshold_for_debate
 
-json.attributes do
-  json.government @parliament.government
-  json.response_threshold @parliament.threshold_for_response
-  json.debate_threshold @parliament.threshold_for_debate
-end
-json.constituencies @parliament.constituencies.each do |constituency|
-                        json.constituency constituency.name
-                        json.ons_code constituency.ons_code
-                        json.start_date constituency.start_date
-                        json.end_date constituency.end_date
+json.constituencies do
+  @constituencies.each do |constituency|
+    json.set! constituency.ons_code do
+      json.constituency constituency.name
+      json.ons_code constituency.ons_code
+      json.start_date constituency.start_date
+      json.end_date constituency.end_date
+    end
+  end
 end
--- spec/requests/parliaments_spec.rb
+++ spec/requests/parliaments_spec.rb
@@ -35,30 +35,97 @@ RSpec.describe "API request to display parliaments", type: :request, show_except
   end
 
   describe "data fields" do
-    it "returns a list of parliaments", :skip_before_hook do
-      parliament = FactoryBot.create(:parliament, :dissolved, state_opening_at: "2021/05/23", opening_at: "2021/05/30", dissolution_at: "2022/06/30", period: "2021-2022", archived_at: "2022/08/30")
-      parliament_2 = FactoryBot.create(:parliament, :dissolved, state_opening_at: "2022/05/23", opening_at: "2022/05/30", dissolution_at: "2023/06/30", period: "2023-2024", archived_at: "2023/08/30")
+    before do
+      FactoryBot.create(
+        :parliament, :dissolved,
+        state_opening_at: "2021/05/23",
+        opening_at: "2021/05/30",
+        dissolution_at: "2022/06/30",
+        archived_at: "2022/08/30"
+      )
 
+      FactoryBot.create(
+        :parliament, :dissolved,
+        state_opening_at: "2022/05/23",
+        opening_at: "2022/05/30",
+        dissolution_at: "2023/06/30",
+        archived_at: "2023/08/30"
+      )
+    end
+
+    subject(:json) { JSON.parse(response.body) }
+
+    it "returns a list of parliaments" do
       get "/parliaments.json"
 
-      expect(JSON.parse(response.body).first).to match({"debate_threshold"=>100000, "dissolution_at"=>"2023-06-30T00:00:00.000+01:00", "government"=>"Conservative", "period"=>"2022-2023", "response_threshold"=>10000})
+      expect(json).to match([
+        {
+          "period"             => "2022-2023",
+          "dissolution_at"     => "2023-06-30T00:00:00.000+01:00",
+          "government"         => "Conservative",
+          "response_threshold" => 10000,
+          "debate_threshold"   => 100000
+        },
+        {
+          "period"             => "2021-2022",
+          "dissolution_at"     => "2022-06-30T00:00:00.000+01:00",
+          "government"         => "Conservative",
+          "response_threshold" => 10000,
+          "debate_threshold"   => 100000
+        }
+      ])
     end
   end
 
-  describe "data fields for constituencies", :skip_before_hook do
+  describe "data fields for constituencies" do
+    before do
+      parliament = FactoryBot.build(
+        :parliament, :dissolved,
+        state_opening_at: "2021/05/23",
+        opening_at: "2021/05/30",
+        dissolution_at: "2022/06/30",
+        archived_at: "2022/08/30"
+      )
+
+      parliament.constituencies << FactoryBot.build(:constituency, name: "Altrincham and Sale West", ons_code: "E14000532")
+      parliament.constituencies << FactoryBot.build(:constituency, name: "Aldershot", ons_code: "E14000530")
+      parliament.constituencies << FactoryBot.build(:constituency, name: "Aldridge-Brownhills", ons_code: "E14000531")
+
+      parliament.save!
+    end
+
+    subject(:json) { JSON.parse(response.body) }
+
     it "returns a list of constituencies for a specific parliament" do
-      parliament = FactoryBot.create(:parliament, :dissolved, state_opening_at: "2021/05/23", opening_at: "2021/05/30", dissolution_at: "2022/06/30", period: "2021-2022", archived_at: "2022/08/30")
-      constituency = FactoryBot.create(:constituency, name: "Buckinghamshire", ons_code: "E00000001")
-      constituency_2 = FactoryBot.create(:constituency, name: "Aberafan")
-
-      parliament.constituencies << constituency
-      parliament.constituencies << constituency_2
-
       get "/parliaments/2021-2022.json"
 
-      expect(JSON.parse(response.body)["period"]).to match("2021-2022")
-      expect(JSON.parse(response.body)["constituencies"].length).to match(2)
-      expect(JSON.parse(response.body)["constituencies"].first).to match({"constituency"=>"Buckinghamshire", "end_date"=>nil, "ons_code"=>"E00000001", "start_date"=>nil})
+      expect(json).to match({
+        "period"             => "2021-2022",
+        "dissolution_at"     => "2022-06-30T00:00:00.000+01:00",
+        "government"         => "Conservative",
+        "response_threshold" => 10000,
+        "debate_threshold"   => 100000,
+        "constituencies"     => {
+          "E14000530" => {
+            "constituency" =>"Aldershot",
+            "ons_code"     => "E14000530",
+            "start_date"   => "2010-04-13",
+            "end_date"     => nil
+          },
+          "E14000531" => {
+            "constituency" => "Aldridge-Brownhills",
+            "ons_code"     => "E14000531",
+            "start_date"   => "2010-04-13",
+            "end_date"     => nil
+          },
+          "E14000532" => {
+            "constituency" => "Altrincham and Sale West",
+            "ons_code"     => "E14000532",
+            "start_date"   => "2010-04-13",
+            "end_date"     => nil
+          }
+        }
+      })
     end
   end
 end
