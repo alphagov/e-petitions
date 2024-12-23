da9852da2d1c36a5a6b36c3e9935e531189e3f80
Andrew White <andrew.white@unboxed.co>
2024-12-23 22:08:00 +0000

    Use dnsmasq for common hostnames inside and outside docker

--- spec/mailers/previews/feedback_mailer_preview.rb
+++ spec/mailers/previews/feedback_mailer_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/feedback_mailer
+# Preview all emails at http://petitions.localhost:3000/rails/mailers/feedback_mailer
 class FeedbackMailerPreview < ActionMailer::Preview
   def send_feedback
     FeedbackMailer.send_feedback(Feedback.last)
