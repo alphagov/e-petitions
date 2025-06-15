66609445600e27152253af2d1d0631389df0fbd7
Andrew White <andrew.white@unboxed.co>
2025-06-15 23:13:12 +0100

    Convert signature counter to vanilla javascript

---
+++ app/assets/javascripts/modules/signature-counter.js
@@ -0,0 +1,75 @@
+class SignatureCounter {
+  constructor(container, responseThreshold, debateThreshold, interval) {
+    this.responseThreshold = responseThreshold;
+    this.debateThreshold = debateThreshold;
+    this.jsonUrl = window.location.href + '/count.json';
+    this.signatureCount = container.querySelector('.signature-count-number .count');
+    this.signatureGoal = container.querySelector('.signature-count-goal');
+    this.progressBar = container.querySelector('progress');
+    this.formatter = new Intl.NumberFormat('en-GB', { maximumFractionDigits: 0 });
+
+    setInterval(async () => { await this.fetchCount() }, interval);
+  }
+
+  async fetchCount() {
+    try {
+      const response = await fetch(this.jsonUrl);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const current = this.progressBar.value;
+
+      if (data.signature_count != current) {
+        this.countTo(current, data.signature_count)
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  format(number) {
+    return this.formatter.format(number);
+  }
+
+  getThreshold(signatureCount) {
+    if (signatureCount >= this.responseThreshold) {
+      return this.debateThreshold;
+    } else {
+      return this.responseThreshold;
+    }
+  }
+
+  render() {
+    const threshold = this.getThreshold(this.value);
+
+    this.signatureCount.textContent = this.format(this.value);
+    this.signatureGoal.textContent = this.format(threshold);
+    this.progressBar.value = Math.floor(this.value);
+    this.progressBar.max = threshold;
+  }
+
+  countTo(currentCount, newCount) {
+    this.value = currentCount;
+    this.loopCount = 0;
+    this.increment = (newCount - currentCount) / 20;
+
+    this.interval = setInterval(() => {
+      this.value += this.increment;
+      this.loopCount++;
+      this.render();
+
+      if (this.loopCount >= 20) {
+        clearInterval(this.interval);
+
+        this.value = newCount
+        this.render();
+      }
+    }, 50);
+  }
+}
+
+window.PETS = window.PETS || {};
+window.PETS.SignatureCounter = SignatureCounter;
