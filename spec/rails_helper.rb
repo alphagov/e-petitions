dafe26f11930be350db283f0fe3dcd0d154415f2
Andrew White <andrew.white@unboxed.co>
2025-09-04 13:20:38 +0100

    Upgrade to Rails 8.0.2.1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ WebMock.disable_net_connect!(
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
+Rails.application.reload_routes_unless_loaded
 
 RSpec.configure do |config|
   config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
