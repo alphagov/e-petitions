74a8588c5d85c3227210516a37d613984e8b112a
Andrew White <andrew.white@unboxed.co>
2025-11-14 09:20:46 +0000

    Don't use DOMContentLoaded
    This causes the request to be chained and we're already at the end anyway.

--- app/assets/javascripts/sharing.js
+++ app/assets/javascripts/sharing.js
@@ -3,11 +3,9 @@ import Sharing from './modules/sharing'
 window.PETS = window.PETS || {};
 window.PETS.Sharing = Sharing;
 
-window.addEventListener('DOMContentLoaded', (event) => {
-  const shareButton = document.getElementById('shareButton');
-  const copyLinkButton = document.getElementById('copyLinkButton');
+const shareButton = document.getElementById('shareButton');
+const copyLinkButton = document.getElementById('copyLinkButton');
 
-  if (shareButton && copyLinkButton) {
-    new PETS.Sharing(shareButton, copyLinkButton);
-  }
-});
+if (shareButton && copyLinkButton) {
+  new PETS.Sharing(shareButton, copyLinkButton);
+}
