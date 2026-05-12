2da45d3e066b8e57f48714b7bd40862b482a67af
Andrew White <andrew.white@unboxed.co>
2026-05-12 09:28:16 +0100

    Reset sequences in cucumber hooks as well as rspec
    Since cucumber runs in a different container than rspec on CI it wasn't
    getting reset and we were seeing current and archived petitions with
    the same number which doesn't happen in reality unless we're in the
    middle of archiving the petitions ahead of a new parliament.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,15 @@
+BeforeAll do
+  ActiveRecord::Base.with_connection do |connection|
+    connection.execute <<~SQL
+      ALTER SEQUENCE archived_petitions_id_seq MAXVALUE 699999
+    SQL
+
+    connection.execute <<~SQL
+      ALTER SEQUENCE petitions_id_seq START WITH 700000 RESTART WITH 700000 MINVALUE 700000
+    SQL
+  end
+end
+
 Before do
   default_url_options[:protocol] = 'https'
 end
